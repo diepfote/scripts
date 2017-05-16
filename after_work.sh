f7da71701e22305aec23fd7359962ba00e8caaae
Florian Begusch <florian.begusch@gmail.com>
2017-05-16 17:58:06 +0200

    Fix start_work spacing; add after_work script

---
+++ after_work.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SEP=$(printf "=================\n\n")
+
+echo git pull c
+echo
+cd ~/Documents/c/; git pull
+echo $SEP
+
+echo git pull cpp
+echo
+cd ~/Documents/cpp/; git pull
+echo $SEP
+
+echo git pull scripts
+echo
+cd ~/Documents/scripts; git pull
+echo $SEP
+
+echo git pull pythoncode
+echo
+cd ~/Documents/pythoncode; git pull
+echo $SEP
+
+echo git pull Zeiterfassung
+echo
+cd ~/Documents/time_management; git pull
+
