a3aa2900c13912f0438e55ba44ca74dce392ae61
Florian Begusch <florian.begusch@gmail.com>
2017-09-03 00:08:29 +0200

    Fix iphone_new_pictures script

--- iphone_new_pictures.sh
+++ iphone_new_pictures.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-trap "fusermount -u ~/iPhone/" EXIT
-ifuse ~/iPhone/
+upper_dir="/home/$(whoami)"
+trap "fusermount -u $upper_dir/iPhone/" EXIT
+ifuse $upper_dir/iPhone/
 
 sleep 1
 
-cd ~/Documents/iphone_pictures
+LOCAL_PICTURES_DIR=$upper_dir/Documents/iphone_pictures
+IPHONE_DCIM_DIR=$upper_dir/iPhone/DCIM
+cd $LOCAL_PICTURES_DIR
 
 is_newer_available=false
-latest_backup=$(ls -t | head -1)
-latest_file=$(ls -t $latest_backup | head -1)
+latest_backup_dir=$(ls -t | head -1)
+latest_file=$(ls -t $latest_backup_dir/ | head -1)
 
 f_date=$(date '+%m-%d-%Y_%H-%M-%S')
 mkdir $f_date
 
-for f in $(find ~/iPhone/DCIM -name *.JPG); do
-  cp -p $(find $f -newer "/home/flo/Documents/iphone_pictures/$latest_backup/$latest_file") $f_date 2>/dev/null
-#  if find $f -newer "/home/flo/Documents/iphone_pictures/$latest_backup/$latest_file"; then
- #   cp $f $f_date
-#    is_newer_available=true
-#    echo New file: $f
-#  fi
+for f in $(find $IPHONE_DCIM_DIR -name "*IMG*"); do
+  # FAILS HERE    
+  cp -p $(find $f -newer "$LOCAL_PICTURES_DIR/$latest_backup_dir/$latest_file") "$LOCAL_PICTURES_DIR/$f_date" 2>/dev/null
 done
 
-is_new_files=$(ls $f_date)
+cd "$LOCAL_PICTURES_DIR/$f_date"
+is_new_files=$(ls)
 
 if $is_new_files; then
+  cd ..
   rm -rf $f_date
   echo No new files found.
   echo
