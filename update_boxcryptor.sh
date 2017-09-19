4b331afbfe5f36a8df6dbed32a7c304b0d40efa6
Florian Begusch <florian.begusch@gmail.com>
2017-09-19 06:18:19 +0200

    Fix update_boxcryptor script

--- update_boxcryptor.sh
+++ update_boxcryptor.sh
@@ -4,16 +4,18 @@ cd ~/Desktop/boxcr/
 
 current_boxcryptor=$(curl -sI https://ptc.secomba.com/api/boxcryptor/linuxPortable/latest | grep Location | cut -d ':' -f3 | cut -d '/' -f7 | sed "s/\r//")
 
-last_in_filesystem=$(find -printf "%TY-%Tm-%Td %TT %p\n" | sort -n | grep tar.gz | head -1 | cut -d ' ' -f3 | cut -d '/' -f2)
+last_in_filesystem=$(find -printf "%TY-%Tm-%Td %TT %p\n" | sort -nr | grep tar.gz | head -1 | cut -d ' ' -f3 | cut -d '/' -f2) 
 
 if echo $last_in_filesystem | grep $current_boxcryptor 1>/dev/null; then
   echo No new boxcryptor version.
+  echo Current version is: $last_in_filesystem
+  echo
 else
   curl -sL https://ptc.secomba.com/api/boxcryptor/linuxPortable/latest -o $current_boxcryptor
 
   temp=temp
   mkdir $temp
-  mv $current_boxcryptor $temp
+  cp $current_boxcryptor $temp
   cd $temp
   tar -xf $current_boxcryptor
   cd ..
@@ -25,9 +27,8 @@ else
   rm README.txt
   rm Boxcryptor_Portable.sh
   
-  # delete oldest tar.gz version
-  rm $(find -printf "%TY-%Tm-%Td %TT %p\n" | sort -n | grep tar.gz | head -1 | cut -d '/' -f2)
-
+  # delete oldest tar.gz version (3rd .tar.gz file is deleted - sorted by newest)
+  rm $(find -printf "%TY-%Tm-%Td %TT %p\n" | sort -nr | grep tar.gz | sed -n 4p | cut -d '/' -f2) 
 
   mv $temp/README.txt .
   mv $temp/Boxcryptor_Portable.sh .
