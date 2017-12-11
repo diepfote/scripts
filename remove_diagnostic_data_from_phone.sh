8a3b7a70ab95bba9b7dac58cacc612d5ccd4b9d3
Florian Begusch <florian.begusch@gmail.com>
2017-12-12 00:55:19 +0100

    Fix remove_diagnostic_data_from_phone script; add error message to 1iphone_new_pictures_and_backup script

--- remove_diagnostic_data_from_phone.sh
+++ remove_diagnostic_data_from_phone.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-t=(mkdir -d)
+
+echo
+echo -e "\033[1;33mStart remove diagnostics\033[0m"
+t=$(mktemp -d)
 
 idevicecrashreport -e $t
 rm -rf $t
+
+echo -e "\033[1;33mEnd remove diagnostics\033[0m"
 
