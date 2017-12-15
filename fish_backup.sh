37a77e1ace340849b506bea529bd452709b7b885
Florian Begusch <florian.begusch@gmail.com>
2017-12-15 11:48:39 +0100

    12-15-2017 11:48:39

--- fish_backup.sh
+++ fish_backup.sh
@@ -3,14 +3,14 @@
 user=`cut -d : -f 1 /etc/passwd | grep flo`
 user_dir=/home/$user
 
-fish_backup_dir=`sudo find $user_dir -name fish_backup 2>/dev/null`
+fish_backup_dir=`find $user_dir -name fish_backup 2>/dev/null`
+
+# Remove 10th file
+trap "rm -f $fish_backup_dir/`ls -t $fish_backup_dir | sed -n 10p` 2>/dev/null" EXIT
 
 # Get parent directory
 fish_dir=`dirname $fish_backup_dir`
 f_date=$(date '+%m-%d-%Y_%H-%M-%S')
-
-# Remove 10th file
-trap "chown -R $user:$user $fish_backup_dir; rm -f $fish_backup_dir/`ls -t $fish_backup_dir | sed -n 10p` 2>/dev/null" EXIT
 
 cp $fish_dir/fish_history $fish_backup_dir/fish_history$f_date
 
