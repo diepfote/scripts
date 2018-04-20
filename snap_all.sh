42b3dec9355eadb2ab6752ce8b4cf8367a20054a
Florian Begusch <florian.begusch@gmail.com>
2018-04-20 08:28:52 +0200

    Alter size for lvm snapshots

--- snap_all.sh
+++ snap_all.sh
@@ -4,22 +4,22 @@ vol_group_mapper=/dev/mapper/VolGroup00
 d=$(date '+%Y-%m-%d_%H-%M-%S')
 
 dir=root
-size=1GB
+size='2.5GB'
 sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
 
 dir=boot
-size=152MB
+size=156MB
 sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
 
 dir=home
-size=1GB
+size='2.5GB'
 sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
 
 dir=var
-size=1GB
+size='2.5GB'
 sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
 
 dir=opt
-size=1GB
+size='2.5GB'
 sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
 
