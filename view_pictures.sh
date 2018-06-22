ccc597ae4b15930ca8b672f5ed1c0d019f008862
Florian Begusch <florian.begusch@gmail.com>
2018-06-22 19:28:33 +0200

    misc

--- view_pictures.sh
+++ view_pictures.sh
@@ -5,7 +5,7 @@ PICT_DIR=~/Documents/iphone_pictures/
 trap "sudo chown -R root:root $PICT_DIR" EXIT
 
 sudo chown -R $USER:$USER $PICT_DIR
-xdg-open $PICT_DIR
+xdg-open $PICT_DIR &>/dev/null
 
 sleep infinity
 
