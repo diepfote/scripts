4747396291f743dcbcbf327c8c7d93f795371e7c
Florian Begusch <florian.begusch@gmail.com>
2018-11-10 17:42:42 +0100

    Change order of snaps

--- snap_all.sh
+++ snap_all.sh
@@ -5,16 +5,16 @@ vol_group_mapper=/dev/mapper/VolGroup00
 d=$(date +%FT%T%Z | sed 's/:/--/g')
 d="${d//:/-}"
 
+dir=home
+size='5GB'
+sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
+
 dir=root
 size='1.5GB'
 sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
 
 dir=boot
 size=356MB
-sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
-
-dir=home
-size='5GB'
 sudo lvcreate -L $size -s -n s_$dir-$d $vol_group_mapper-$dir
 
 dir=var
