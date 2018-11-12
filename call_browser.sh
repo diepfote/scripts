6aeec7d25a742e110642f837e1984e77759d41ca
Florian Begusch <florian.begusch@gmail.com>
2018-11-12 16:59:08 +0100

    Add incognito/private-window option to call_browser.sh calls; move fallback in call_browser.sh to firefox

--- call_browser.sh
+++ call_browser.sh
@@ -4,13 +4,13 @@ brave=$2
 
 if [ -z $brave ]; then
   # call chrome
-  vivaldi-stable $TMP_FILE 2> /dev/null 1> /dev/null &
+  vivaldi-stable --incognito $TMP_FILE 2> /dev/null 1> /dev/null &
 else
   brave $TMP_FILE 2> /dev/null 1> /dev/null &
 fi
 
 if [ "$?" -eq 127 ] || [ "$(which vivaldi-stable)" == "" ] ; then # google-chrome-stable not found
-	chromium-browser $TMP_FILE 2> /dev/null 1> /dev/null
+  firefox --private-window $TMP_FILE 2> /dev/null 1> /dev/null
 fi
 
 sleep 2
