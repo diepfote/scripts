38ea69af2baa4c8e6777e6335df0795bb20dccf1
Florian Begusch <florian.begusch@gmail.com>
2018-11-13 13:36:34 +0100

    Use firefox --private-window for all script based logins

--- call_browser.sh
+++ call_browser.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 TMP_FILE=$1
-brave=$2
+#brave=$2
 
-if [ -z $brave ]; then
-  # call chrome
-  vivaldi-stable --incognito $TMP_FILE 2> /dev/null 1> /dev/null &
-else
-  brave $TMP_FILE 2> /dev/null 1> /dev/null &
-fi
+#if [ -z $brave ]; then
+  ## call chrome
+  #vivaldi-stable --incognito $TMP_FILE 2> /dev/null 1> /dev/null &
+#else
+  #brave $TMP_FILE 2> /dev/null 1> /dev/null &
+#fi
 
-if [ "$?" -eq 127 ] || [ "$(which vivaldi-stable)" == "" ] ; then # google-chrome-stable not found
-  firefox --private-window $TMP_FILE 2> /dev/null 1> /dev/null
-fi
+#if [ "$?" -eq 127 ] || [ "$(which vivaldi-stable)" == "" ] ; then # google-chrome-stable not found
+  #firefox --private-window $TMP_FILE 2> /dev/null 1> /dev/null
+#fi
+
+firefox --private-window $TMP_FILE 2> /dev/null 1> /dev/null
 
 sleep 2
 
