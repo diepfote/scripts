d591d8ae5ddb1b6145a1258cb997c720eb7a3530
Florian Begusch <florian.begusch@gmail.com>
2018-12-17 12:32:40 +0100

    Add rm_pass_files.sh

---
+++ rm_pass_files.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+user=$(cut -d : -f 1 /etc/passwd | grep flo)
+user_dir=/home/$user  
+
+script_dir=$user_dir/Documents/scripts
+pass_dir=$user_dir/Documents/passwds
+
+if [ ! -d $script_dir ]; then
+  script_dir=$user_dir/Docs/scripts
+  pass_dir=$user_dir/Dokumente/passwds
+fi
+
+for part_file in "$@"
+do
+  for files in "$(find $pass_dir -iname "*$part_file*.gpg")"
+  do 
+    for file in $(echo -e $files)
+    do
+      echo -n "Delete $file (y/n): "
+      read confirmation
+
+      if [ $confirmation == "y" ]; then
+        echo "[>] Deleting $file."
+        rm $file
+      fi
+    done
+  done
+done
+
