2cf8b48584c92c179a5e377f0df6a52f16febeb4
Florian Begusch <florian.begusch@gmail.com>
2018-12-18 19:59:04 +0100

    Fix owncloud syncing (hopefully)

--- view_pass_file.sh
+++ view_pass_file.sh
@@ -3,15 +3,15 @@
 user=$(cut -d : -f 1 /etc/passwd | grep flo)
 user_dir=/home/$user  
 
-dir=$user_dir/Documents
+script_dir=$user_dir/Documents/scripts
+pass_dir=$user_dir/Documents/passwds
 
-if [ ! -d $dir ]; then
-  set dir $user_dir/Dokumente
+if [ ! -d $script_dir ]; then
+  script_dir=$user_dir/Docs/scripts
+  pass_dir=$user_dir/Dokumente/passwds
 fi
 
-pass_dir=$dir/passwds
-
-pass=$($dir/scripts/read_pass.sh)
+pass=$($script_dir/read_pass.sh)
 
 file=$(find $pass_dir -iname "*$1*.gpg")
 echo $pass | gpg -d --batch --passphrase-fd 0 $file 2>/dev/null
