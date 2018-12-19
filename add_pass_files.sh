583293ec9ea2772159886f69a4151b728ef54115
Florian Begusch <florian.begusch@gmail.com>
2018-12-19 14:29:00 +0100

    Add add_pass_files.sh and dump_pass_files.sh

---
+++ add_pass_files.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+function check_error
+{
+  error_code="$1"
+  where=$2
+  RED=$3
+  NC=$4
+  if [ "$error_code" != 0 ]; then
+    echo -e "${RED}Error at $where! EC: $error_code${NC}" 1>&2
+    exit $error_code
+  fi
+}
+
+file_dir=$1
+
+user=$(cut -d : -f 1 /etc/passwd | grep flo)
+user_dir=/home/$user  
+
+script_dir=$user_dir/Documents/scripts
+pass_dir=$user_dir/Documents/passwds
+
+if [ ! -d $script_dir ]; then
+  script_dir=$user_dir/Docs/scripts
+  pass_dir=$user_dir/Dokumente/passwds
+fi
+
+pass=$($script_dir/read_pass.sh)
+pass_second=$($script_dir/read_pass.sh)
+
+
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+if [ $pass != $pass_second ]; then
+  echo -en "$RED"; echo -en "Passwords do not match!$NC"; echo
+else
+
+  files=$(find $file_dir -maxdepth 1 -type f)
+  # DEBUG
+  #echo -e "$files"
+
+  for file in $(echo -e $files) 
+  do
+    echo -e "----------------------\nAdding $(basename $file)\n"
+    
+    echo $pass | gpg --no-symkey-cache -c --batch --passphrase-fd 0 $file
+    #echo $pass | gpg --cipher-algo AES256 --no-symkey-cache -c --batch --passphrase-fd 0 $file
+    check_error "$?" gpg $RED $NC
+
+    mv $file.gpg $pass_dir
+    check_error "$?" mv $RED $NC 
+    chmod 600 $pass_dir/*
+    check_error "$?" chmod $RED $NC 
+
+    rm $file
+    check_error "$?" rm $RED $NC 
+    
+    echo -en "$GREEN"; echo -en "Successfully added $(basename $file)!$NC"; echo
+
+    echo -e "\n----------------------\n"
+  done
+fi
+
---
+++ dump_pass_files.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+user=$(cut -d : -f 1 /etc/passwd | grep flo)
+user_dir=/home/$user  
+
+script_dir=$user_dir/Documents/scripts
+pass_dir=$user_dir/Documents/passwds
+temp_dir=$(mktemp -d)
+
+if [ ! -d $script_dir ]; then
+  script_dir=$user_dir/Docs/scripts
+  pass_dir=$user_dir/Dokumente/passwds
+fi
+
+pass=$($script_dir/read_pass.sh)
+
+
+for files in "$(find $pass_dir -iname "*.gpg")"
+do 
+  for file in $(echo -e $files) 
+  do
+    echo $pass | gpg --no-symkey-cache -d --batch --passphrase-fd 0 $file > $temp_dir/$(basename $file | sed 's/.gpg//')
+    #echo $pass | gpg --cipher-algo AES256 --no-symkey-cache -d --batch --passphrase-fd 0 $file > $temp_dir/$(basename $file | sed 's/.gpg//')
+    echo -e "\n----------------------\n"
+  done
+done
