4f3b69846e7c0b1130f05cb36c2be8c4a70c628d
Florian Begusch <florian.begusch@gmail.com>
2019-01-08 12:06:34 +0100

    Add sed entry for draw.io on Ubuntu

--- custom_firejail_hook_script.sh
+++ custom_firejail_hook_script.sh
@@ -3,6 +3,7 @@
 firecfg &>/dev/null
 ln -s /usr/bin/firejail /usr/local/bin/draw.io
 sed -i 's#/usr/bin/draw.io#/usr/local/bin/draw.io#g' /usr/share/applications/draw.io.desktop
+sed -i 's#/opt/draw.io/draw.io#/usr/local/bin/draw.io#g' /usr/share/applications/draw.io.desktop
 
 ln -s /usr/bin/firejail /usr/local/bin/bibtex
 ln -s /usr/bin/firejail /usr/local/bin/latex
