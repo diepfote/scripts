df7dd22abbb4c8b2bff41ed51bde8b4381ad6f33
Florian Begusch <florian.begusch@gmail.com>
2019-01-13 20:32:44 +0100

    Add functionality to view_pictures

--- view_dir.sh
+++ view_dir.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 DIR=~/Documents/$1
+no_xdg=$2
 
-trap "sudo chown -R root:root $DIR" EXIT
+trap 'sudo chown -R root:root "$DIR"' EXIT
 
-sudo chown -R $USER:$USER $DIR
-xdg-open $DIR &>/dev/null
+sudo chown -R $USER:$USER "$DIR"
+if [ "$?" != "0" ]; then
+  echo Insufficient permissions, exiting.
+  exit
+fi
+
+if [ -z $no_xdg ]; then
+  xdg-open $DIR &>/dev/null
+fi
 
 sleep infinity
 
