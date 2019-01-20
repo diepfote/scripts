c26b2f27185a99a4f1ebea90525b7a4e72dee504
Florian Begusch <florian.begusch@gmail.com>
2019-01-21 00:14:00 +0100

    Fix read_toml_setting script

--- read_toml_setting.sh
+++ read_toml_setting.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-echo $(cat $1 | grep $2 | sed 's#[A-Za-z0-9_-]*=##' | head -n 1)
+#echo -e "$(cat $1 | grep -v "^#.*")"
+echo -e "$(cat $1 | grep -v "^#.*" | grep "$2" | sed 's/[A-Za-z0-9_-]*=//' | head -n 1)"
 
