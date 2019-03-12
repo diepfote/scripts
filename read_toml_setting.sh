b839e29d43167e4487e6bb7b6e1b36a1c9d17d72
Florian Begusch <florian.begusch@gmail.com>
2019-03-13 00:12:44 +0100

    Fix edge cases for read_toml_setting.sh

--- read_toml_setting.sh
+++ read_toml_setting.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #echo -e "$(cat $1 | grep -v "^#.*")"
-echo -e "$(cat $1 | grep -v "^#.*" | grep "$2" | sed 's/[A-Za-z0-9_-]*=//' | head -n 1)"
+#echo -e "$(cat $1 | grep -v "^#.*" | grep "$2" | sed 's/[A-Za-z0-9_-]*=//' | head -n 1)"
+echo -e "$(cat $1 | grep -v "^#.*" | grep "$2" | python -c 'print(input().split("=", 1)[1])')"
 
