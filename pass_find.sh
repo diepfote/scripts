b6404250c2433ddef837f72bc4b21560aca1e00c
Florian Begusch <florian.begusch@gmail.com>
2019-03-19 13:40:11 +0100

    Add pass_find script

---
+++ pass_find.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+user=$(cut -d : -f 1 /etc/passwd | grep flo)
+user_dir=/home/$user  
+
+pass_dir=$user_dir/Documents/passwds
+
+if [ ! -d $script_dir ]; then
+  pass_dir=$user_dir/Dokumente/passwds
+fi
+for part_file in "$@"
+do
+  for files in "$(find $pass_dir -iname "*$part_file*.gpg")"
+  do 
+    echo -e "----------------------\n[Matches]: \n"
+    for file in $(echo -e $files)
+    do
+      echo -e "$(basename $file)\n"
+    done
+      echo -e "----------------------\n"
+  done
+done
+
