0d58db5447e25dd9660b78e1c403f597fd8673ea
Florian Begusch <florian.begusch@gmail.com>
2019-05-01 22:33:04 +0200

    add writing options.cnf file for Android inside the generate_self_signed_cert_for_Android.sh script

--- generate_self_signed_cert_for_Android.sh
+++ generate_self_signed_cert_for_Android.sh
@@ -6,8 +6,13 @@ cd $1
 openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
 # generate CA.pem
 openssl req -new -days 365 -key key.pem  -out CA.pem
+
+options_file=options.cnf
+# for Android root CA
+echo 'basicConstraints=CA:true' > $options_file
+
 # generate crt file
-openssl x509 -req -days 365 -in CA.pem  -signkey key.pem  -extfile ./options.cnf -out CA.crt
+openssl x509 -req -days 365 -in CA.pem  -signkey key.pem  -extfile $options_file -out CA.crt
 # convert to DER format for Android
 openssl x509 -inform PEM -outform DER -in CA.crt -out CA.der.crt
 
