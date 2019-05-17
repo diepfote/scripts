215da1783942c39883e155fc43ceafb024a54991
Florian Begusch <florian.begusch@gmail.com>
2019-05-17 17:08:41 +0200

    Changes for fix Exec statements for user florian

--- custom_signal_hook_script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rm -f /usr/share/applications/signal-desktop-tray.desktop
-sed -i 's#/opt/Signal/signal-desktop#/usr/local/bin/signal-desktop#g' /usr/share/applications/signal-desktop.desktop
-
