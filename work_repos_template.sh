a2511394e1afb72e5c74999417c0173167bc524f
Florian Begusch <florian.begusch@gmail.com>
2019-05-18 02:00:20 +0200

    Add work_repos_template.sh

---
+++ work_repos_template.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function arbitrary_command
+{
+  dir=$1
+  echo "$dir"
+  if [ -d $dir/.git ]; then
+    cd $dir 
+    # execute command (skipping first parameter)
+    "${@:2}"
+  else
+    echo -en "$YELLOW"; echo -en "No .git dir in $dir$NC\n"
+  fi
+  echo
+}
+
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+arbitrary_command $@
+
