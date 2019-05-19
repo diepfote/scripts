946a62361ca8e0a4c8c16212ceab0379c9960024
Florian Begusch <florian.begusch@gmail.com>
2019-05-19 21:13:07 +0200

    Add gsettings_script.sh

---
+++ gsettings_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# gsettings set org.gnome.system.proxy mode manual
+# gsettings set org.gnome.system.proxy.socks host '127.0.0.1'
+# gsettings set org.gnome.system.proxy.socks port 7000
+
+gsettings set org.cinnamon.desktop.default-applications.terminal exec uxterm
+gsettings set org.gnome.desktop.media-handling automount false
+gsettings set org.gnome.desktop.wm.keybindings raise "['F1']"
+
+# nemo / nautlis
+gsettings set org.gnome.desktop.background show-desktop-icons false
+gsettings set org.nemo.desktop show-desktop-icons true
+
