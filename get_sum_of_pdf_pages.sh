879b5ce8691dad197242a02203aae4ee1602e3b6
Florian Begusch <florian.begusch@gmail.com>
2019-06-02 18:08:43 +0200

    Fix get_sum_of_pdf_pages

--- get_sum_of_pdf_pages.sh
+++ get_sum_of_pdf_pages.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
-# This script either takes a path to a folder containing PDF files or
-# assumes the current directory is where relevant PDF files reside
-#
-directory="$@"
-if [ -z "$directory" ]; then
-  directory=.
-fi
-
 # TODO should not run find twice
-find "$directory" -type f -iname '*.pdf' -exec bash -c 'echo "{}"; pdfinfo "{}"  2>/dev/null | grep 'Pages: '; echo' \;
-calc_command="$(find "$directory" -type f -iname '*.pdf' -exec bash -c 'pdfinfo "{}"  2>/dev/null | grep 'Pages: ' | cut -d ':' -f2' \; | sed 's#Pages: ##g' | tr '\n' '+') 0"
+result_for_each="$(find "$@" -type f -iname '*.pdf' -exec bash -c 'echo -n "{}"; pdfinfo "{}"  2>/dev/null | grep "Pages:" | sed "s#Pages:          #\t\t#g"' \;)"
+calc_command="$(find "$@" -type f -iname '*.pdf' -exec bash -c 'pdfinfo "{}"  2>/dev/null | grep 'Pages: ' | cut -d ':' -f2' \; | sed 's#Pages: ##g' | tr '\n' '+') 0"
 
-echo
-echo -n "SUM: " 
-echo "$calc_command" | bc
+echo -e "$result_for_each" | sort
+
+echo; echo -n "SUM: "; echo "$calc_command" | bc
 
