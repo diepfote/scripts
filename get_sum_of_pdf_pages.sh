7739dec264ce75c3f2344d85778b5fe382c3b77b
Florian Begusch <florian.begusch@gmail.com>
2019-06-14 02:34:14 +0200

    generalize get_sum_of_pages.sh (filename regex is a parameter now)

--- get_sum_of_pdf_pages.sh
+++ get_sum_of_pdf_pages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # TODO should not run find twice
-result_for_each="$(find "$@" -type f -iname '*.pdf' -exec bash -c 'echo -n "{}"; pdfinfo "{}"  2>/dev/null | grep "Pages:" | sed "s#Pages:##g"' \;)"
-calc_command="$(find "$@" -type f -iname '*.pdf' -exec bash -c 'pdfinfo "{}"  2>/dev/null | grep 'Pages: ' | cut -d ':' -f2' \; | sed 's#Pages: ##g' | tr '\n' '+') 0"
+result_for_each="$(find "$@" -type f -exec bash -c 'echo -n "{}"; pdfinfo "{}"  2>/dev/null | grep "Pages:" | sed "s#Pages:##g"' \;)"
+calc_command="$(find "$@" -type f -exec bash -c 'pdfinfo "{}"  2>/dev/null | grep 'Pages: ' | cut -d ':' -f2' \; | sed 's#Pages: ##g' | tr '\n' '+') 0"
 
 echo -e "$result_for_each" | sort
 
