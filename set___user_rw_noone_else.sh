ef0a09bd228d6bc972748b1b60aad19050a76763
Florian Begusch <florian.begusch@gmail.com>
2019-06-25 07:44:40 +0200

    Prevent errors in set___user_rw_noone_else.sh to return non-zero values

--- set___user_rw_noone_else.sh
+++ set___user_rw_noone_else.sh
@@ -6,6 +6,6 @@ if [ -z "$dir" ]; then
   dir=.
 fi
 
-find "$dir" -type f -exec sh -c 'chmod 600 "{}"' \;
-find "$dir" -type d -exec sh -c 'chmod 700 "{}"' \;
+find "$dir" -type f -exec sh -c 'chmod -f 600 "{}"' \; || true  # do not return non-zero return code
+find "$dir" -type d -exec sh -c 'chmod -f 700 "{}"' \; || true 
 
