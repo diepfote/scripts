4aef23c5cd97063bcb2503e2dee267f9c2e34642
Florian Begusch <florian.begusch@gmail.com>
2019-06-27 23:08:46 +0200

    Set grep do not match binary data

--- get_sum_of_pdf_pages.sh
+++ get_sum_of_pdf_pages.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+#set -x
+
 # TODO should not run find twice
-result_for_each="$(find "$@" -type f -exec bash -c 'echo -n "{}"; pdfinfo "{}"  2>/dev/null | grep "Pages:" | sed "s#Pages:##g"' \;)"
-calc_command="$(find "$@" -type f -exec bash -c 'pdfinfo "{}"  2>/dev/null | grep 'Pages: ' | cut -d ':' -f2' \; | sed 's#Pages: ##g' | tr '\n' '+') 0"
+result_for_each="$(find "$@" -type f -exec bash -c 'echo -n "{}"; pdfinfo "{}"  2>/dev/null | grep "Pages:" 2>/dev/null | sed "s#Pages:##g"' \;)"
+calc_command="$(find "$@" -type f -exec bash -c 'pdfinfo "{}"  2>/dev/null | grep -I 'Pages: ' 2>/dev/null | cut -d ':' -f2' \; | sed 's#Pages: ##g' | tr '\n' '+') 0"
 
 echo -e "$result_for_each" | sort
 
