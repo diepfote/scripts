4cdab0309010597d65ded6e489ed505ed5d055c6
Florian Begusch <florian.begusch@gmail.com>
2019-07-18 13:00:31 +0200

    inotifywait recursively

--- trace_filesystem_changes.sh
+++ trace_filesystem_changes.sh
@@ -5,7 +5,7 @@ event="$2"
 dir_to_watch="$1"
 
 echo -e "[>] starting trace\n  event: '$event'\n  dir: '$(realpath $dir_to_watch)'\n"
-while file=$(inotifywait -e "$event" --format "%w%f" "$dir_to_watch" 2>/dev/null); do
+while file=$(inotifywait -r -e "$event" --format "%w%f" "$dir_to_watch" 2>/dev/null); do
   echo "$file"
 done
 
