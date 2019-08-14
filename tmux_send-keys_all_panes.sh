1a119e03fd13d58a0aa04b32ecc8bcd1a676e480
Florian Begusch <florian.begusch@gmail.com>
2019-08-14 09:45:45 +0200

    Fix tmux_send-keys_all_panes.sh stripping whitespace

--- tmux_send-keys_all_panes.sh
+++ tmux_send-keys_all_panes.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-for pane in $(tmux list-panes -F '#{pane_id}'); do
-  echo "$pane"
-  echo "$@"
-  tmux send-keys -t ${pane} "$@"
+echo -e "----------\ncommand: $@"
+
+current_pane_id="$(tmux list-panes -F '#{pane_id} #{pane_active}' | grep ' 1' | cut -d ' ' -f1)"
+
+for pane_id in $(tmux list-panes -F '#{pane_id}'); do
+
+  if [ "$pane_id" != "$current_pane_id" ]; then
+    echo "pane id: $pane_id"
+    tmux send-keys -t ${pane_id} "$@"
+  fi
 done
+
+echo -e "\n----------"
 
