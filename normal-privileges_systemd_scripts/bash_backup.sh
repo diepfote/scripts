49bf34e10558e4c20ea7135442eda42ed47c9351
Florian Begusch <florian.begusch@gmail.com>
2019-09-30 18:13:35 +0200

    UÃÃpdate fish_backup script; add bash_backup script

---
+++ normal-privileges_systemd_scripts/bash_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+user="$(cut -d : -f 1 /etc/passwd | grep flo | head -n 1)"
+user_dir="/home/$user"
+
+bash_backup_dir="$user_dir/bash_backup"
+[[ -d "$bash_backup_dir" ]] || mkdir "$bash_backup_dir"
+
+# Remove 10th file
+trap "rm -f $bash_backup_dir/$(ls -t $bash_backup_dir | sed -n 10p) 2>/dev/null" EXIT
+
+# Get parent directory
+f_date=$(date +%FT%T%Z | sed 's/:/--/g')
+
+cp "$user_dir/.bash_history" "$bash_backup_dir/bash_history$f_date"
+
+pwd
+
--- normal-privileges_systemd_scripts/fish_backup.sh
+++ normal-privileges_systemd_scripts/fish_backup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-user=`cut -d : -f 1 /etc/passwd | grep flo | head -n 1`
-user_dir=/home/$user
+user="$(cut -d : -f 1 /etc/passwd | grep flo | head -n 1)"
+user_dir=/home/"$user"
 
-fish_backup_dir=$user_dir/.local/share/fish/fish_backup
+fish_backup_dir="$user_dir"/.local/share/fish/fish_backup
 if [ ! -d $fish_backup_dir ]; then
-  fish_backup_dir=$user_dir/.config/fish/fish_backup
+  fish_backup_dir="$user_dir"/.config/fish/fish_backup
 fi
 
 # Remove 10th file
-trap "rm -f $fish_backup_dir/`ls -t $fish_backup_dir | sed -n 10p` 2>/dev/null; $user_dir/Documents/scripts/set___user_rw_noone_else.sh ../$fish_backup_dir" EXIT
+trap "rm -f $fish_backup_dir/$(ls -t $fish_backup_dir | sed -n 10p) 2>/dev/null" EXIT
 
 # Get parent directory
-fish_dir=`dirname $fish_backup_dir`
+fish_dir="$(dirname "$fish_backup_dir")"
 f_date=$(date +%FT%T%Z | sed 's/:/--/g')
 
-cp $fish_dir/fish_history $fish_backup_dir/fish_history$f_date
+cp "$fish_dir/fish_history" "$fish_backup_dir/fish_history$f_date"
 
-echo $(pwd)
+pwd
 
