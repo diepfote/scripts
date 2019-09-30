4c023969228ba5145a7fe3a7374364707eb21823
Florian Begusch <florian.begusch@gmail.com>
2019-09-30 17:14:42 +0200

    Update source-me_colors script

--- source-me_colors.sh
+++ source-me_colors.sh
@@ -1,9 +1,13 @@
-export PURPLE='\033[1;35m'
-export ORANGE='\033[0;33m'
-export CYAN='\033[1;36m'
-export RED='\033[1;31m'
-export YELLOW='\033[1;33m'
-export GREEN='\033[1;32m'
-export LIGHT_GREEN='\033[0;32m'
-export NC='\033[0m'
+if which tput >/dev/null 2>&1; then
+  ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  PURPLE="$(tput setaf 5)"
+  BOLD="$(tput bold)"
+  NC="$(tput sgr0)"  # no color
+fi
 
