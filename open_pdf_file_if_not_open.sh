744b949335120de0aa09af39a639d8e195ec9611
Florian Begusch <florian.begusch@gmail.com>
2019-10-25 07:23:30 +0200

    Add open_pdf_file_if_not_open.sh

---
+++ open_pdf_file_if_not_open.sh
@@ -0,0 +1,9 @@
+open_file_if_not_open ()
+{
+  filename="$1"
+
+  if [ -z "$(ps -ef | grep -v grep | grep zathura | grep "$(basename "$filename")")" ]; then
+    xdg-open "$filename"  1>/dev/null 2>/dev/null
+  fi
+}
+
