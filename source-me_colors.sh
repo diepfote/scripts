66807cb0c9098bff62d89cdc835a1a5f9fc777bd
Florian Begusch <florian.begusch@gmail.com>
2019-11-26 10:54:26 +0100

    Fix colors not exported in source-me

--- source-me_colors.sh
+++ source-me_colors.sh
@@ -2,13 +2,13 @@ if which tput >/dev/null 2>&1; then
   ncolors=$(tput colors)
 fi
 if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-  RED="$(tput setaf 1)"
-  GREEN="$(tput setaf 2)"
-  YELLOW="$(tput setaf 3)"
-  BLUE="$(tput setaf 4)"
-  PURPLE="$(tput setaf 5)"
-  BOLD="$(tput bold)"
-  NC="$(tput sgr0)"  # no color
+  export RED="$(tput setaf 1)"
+  export GREEN="$(tput setaf 2)"
+  export YELLOW="$(tput setaf 3)"
+  export BLUE="$(tput setaf 4)"
+  export PURPLE="$(tput setaf 5)"
+  export BOLD="$(tput bold)"
+  export NC="$(tput sgr0)"  # no color
 fi
 
 # colorize man pages with 'bat'
