49b7e850c31d9e8bccf5a19f2534f34445b2f116
Florian Begusch <florian.begusch@gmail.com>
2019-12-01 19:17:09 +0100

    Fix mount_vdi.sh

--- mount_vdi.sh
+++ mount_vdi.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-vdi_file=$1
+vdi_file="$1"
+mount_partition="$2"
+[[ -z "$mount_partition" ]] && mount_partition=2
+
 temp=$(mktemp -d)
 
-trap "sudo umount $temp; sudo qemu-nbd -d /dev/nbd0" EXIT
+trap "sudo umount '$temp'; sudo qemu-nbd -d /dev/nbd0" EXIT
 
 sudo modprobe -v nbd
 
+set -x
 # mount as disk
-sudo qemu-nbd -c /dev/nbd0 $vdi_file
+sudo qemu-nbd -c /dev/nbd0 "$vdi_file"
 
 # mount partition
-sudo mount /dev/nbd0p2 $temp
+echo -en "$PURPLE"; echo -e "[?] attempting to mount partition 2 (might have to be changed).$NC"
+sudo mount /dev/nbd0p"$mount_partition" "$temp"
+set +x
 
-xdg-open $temp 1>/dev/null 2>/dev/null
+xdg-open "$temp" 1>/dev/null 2>/dev/null
 
 
 sleep infinity
