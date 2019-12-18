8361d5d2567fe22dc753fae924a30440d92da6ab
Florian Begusch <florian.begusch@gmail.com>
2019-12-18 23:20:23 +0100

    Update get_sum_of_pdf_pages

--- get_sum_of_pdf_pages.sh
+++ get_sum_of_pdf_pages.sh
@@ -3,7 +3,7 @@
 #set -x
 
 # TODO should not run find twice
-result_for_each="$(find "$@" -type f -exec bash -c 'echo -n "{}"; pdfinfo "{}"  2>/dev/null | grep "Pages:" 2>/dev/null | sed "s#Pages:##g"' \;)"
+result_for_each="$(find "$@" -type f -exec bash -c 'x=$(pdfinfo "{}"  2>/dev/null | grep "Pages:" 2>/dev/null | sed "s#Pages:##g" | sed "s# ##g" 2>&1); echo -e  "{}\t\t\t$x" ' \;)"
 calc_command="$(find "$@" -type f -exec bash -c 'pdfinfo "{}"  2>/dev/null | grep -I 'Pages: ' 2>/dev/null | cut -d ':' -f2' \; | sed 's#Pages: ##g' | tr '\n' '+') 0"
 
 echo -e "$result_for_each"  | sed 's#^./##' |  sort -n
