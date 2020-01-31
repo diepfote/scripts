4ba4721b3a71aa6da1590912936e08bc3fc02b7d
Florian Begusch <florian.begusch@gmail.com>
2020-01-31 08:07:15 +0100

    Add safe bash template

---
+++ bash-safe-template.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
