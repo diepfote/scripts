a77b3446bbbbbf19bff8962b53e76835427df9a0
Florian Begusch <florian.begusch@gmail.com>
2020-02-17 20:32:08 +0100

    Add completions for helper-functions

---
+++ source-me_completions_edit-function.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+autocomplete_for_different_directory () {
+  FILE_EXCHANGE_DIR=~/.config/fish/functions
+  # Set
+  IFS=$'\n' tmp=( $(compgen -W "$(ls "$FILE_EXCHANGE_DIR")" -- "${COMP_WORDS[$COMP_CWORD]}" ))
+  COMPREPLY=( "${tmp[@]// /\ }" )
+}
+complete -o default -F autocomplete_for_different_directory edit-function
+
