b300cc41c8bab8ea9b5d049d5d0b17b5ff9947b6
Florian Begusch <florian.begusch@gmail.com>
2020-02-17 20:36:44 +0100

    Add completions for helper-functions

--- source-me_completions_ffunctions.sh
+++ source-me_completions_ffunctions.sh
@@ -6,5 +6,7 @@ autocomplete_for_different_directory () {
   IFS=$'\n' tmp=( $(compgen -W "$(ls "$FILE_EXCHANGE_DIR")" -- "${COMP_WORDS[$COMP_CWORD]}" ))
   COMPREPLY=( "${tmp[@]// /\ }" )
 }
+complete -o default -F autocomplete_for_different_directory new-function
 complete -o default -F autocomplete_for_different_directory edit-function
+complete -o default -F autocomplete_for_different_directory duplicate-function
 
