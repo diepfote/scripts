f422a8f4a3921d09f3451e216745910dfa4322ef
Florian Begusch <florian.begusch@gmail.com>
2020-02-17 13:40:35 +0100

    Fix bash-secure-template

--- bash-safe-template.sh
+++ bash-safe-template.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 
 set -o pipefail  # propagate errors
 set -u  # exit on undefined
 set -e  # exit on non-zero return value
 #set -f  # disable globbing/filename expansion
 shopt -s failglob  # error on unexpaned globs
+
 
