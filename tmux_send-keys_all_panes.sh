925b3ee374f108f984a9cfa6b3ec7f20325fe5ab
Florian Begusch <florian.begusch@gmail.com>
2020-02-20 07:59:38 +0100

    Add run command in current pane as well (tmux_send...)

--- tmux_send-keys_all_panes.sh
+++ tmux_send-keys_all_panes.sh
@@ -6,10 +6,8 @@ current_pane_id="$(tmux list-panes -F '#{pane_id} #{pane_active}' | grep ' 1' | 
 
 for pane_id in $(tmux list-panes -F '#{pane_id}'); do
 
-  if [ "$pane_id" != "$current_pane_id" ]; then
-    echo "pane id: $pane_id"
-    tmux send-keys -t ${pane_id} "$@"
-  fi
+  echo "pane id: $pane_id"
+  tmux send-keys -t ${pane_id} "$@"
 done
 
 echo -e "\n----------"
