f53a19f9851da671ff68b61fb0efdc790e0882d8
Florian Begusch <florian.begusch@gmail.com>
2020-02-20 12:40:06 +0100

    Update get_pod function

--- kubernetes/source-me_common_functions.sh
+++ kubernetes/source-me_common_functions.sh
@@ -3,18 +3,19 @@
 
 get_pod()
 {
-  partial_pod_name="$1"
+  local partial_pod_name="$1"
+  do_not_match="$2"
   if [ -n "$namespace" ]; then
-    kubectl get pod -o name -n $namespace | grep $partial_pod_name | head -n 1
+    kubectl get pod -o name -n "$namespace" | grep -vE "$do_not_match"  | grep "$partial_pod_name"  | head -n 1
   else
-      kubectl get pod -o name | grep $partial_pod_name | head -n 1
+    kubectl get pod -o name | grep -vE "$do_not_match"  | grep "$partial_pod_name"  | head -n 1
   fi
 }
 
 get_pod_volumes()
 {
-  separator="$1"
-  
+  local separator="$1"
+
   [[ -z "$pod" ]] && pod="$2"
 
   if [ -n "$namespace" ]; then
