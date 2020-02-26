ffa7760fd11ea1b62b436ef4280fd43c5667c9f7
Florian Begusch <florian.begusch@gmail.com>
2020-02-27 00:03:55 +0100

    Add report-brew-pkg.sh

---
+++ report-brew-pkgs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
+dir=~/Documents/misc/mac-os
+[ ! -d "$dir" ] && mkdir "$dir"
+
+#brew info --installed --json | jq .[].name | sed 's#"##g' > ~/Documents/misc/mac-os/brew-pkgs.txt
+brew list > ~/Documents/misc/mac-os/brew-pkgs.txt
+brew cask list > ~/Documents/misc/mac-os/brew-cask-pkgs.txt
+
+username="$(read_toml_setting ~/Documents/config/fastmail.conf username)"
+rclone sync --delete-excluded -v ~/Documents/misc/mac-os/ 'fastmail:'$username'.fastmail.com/files/-configs/mac-os'
+
