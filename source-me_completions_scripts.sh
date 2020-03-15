a502eab4a7b2a9046c7d6b554e2ee489d18b306e
Florian Begusch <florian.begusch@gmail.com>
2020-03-15 20:34:38 +0100

    Fix script completions not taking args

--- source-me_completions_scripts.sh
+++ source-me_completions_scripts.sh
@@ -4,9 +4,9 @@ script_dir="$HOME/Documents/scripts"
 source "$script_dir/source-me_common-functions.sh"
 export DIR_TO_COMPLETE="$script_dir"
 
-complete -o filenames -F _complete_files_and_dirs_helper new-script
-complete -o filenames -F _complete_files_and_dirs_helper edit-script
-complete -o filenames -F _complete_files_and_dirs_helper duplicate-script
-complete -o filenames -F _complete_files_and_dirs_helper mv-script
-complete -o filenames -F _complete_files_and_dirs_helper rm-script
+complete -o filenames -F _complete_files_and_dirs new-script
+complete -o filenames -F _complete_files_and_dirs edit-script
+complete -o filenames -F _complete_files_and_dirs duplicate-script
+complete -o filenames -F _complete_files_and_dirs mv-script
+complete -o filenames -F _complete_files_and_dirs rm-script
 
