a603013e4606774e882f9059cc9723dd4a1a10f3
Florian Begusch <florian.begusch@gmail.com>
2020-03-16 07:26:31 +0100

    Prettify logging for commit-in-dir.sh

--- commit-in-dir.sh
+++ commit-in-dir.sh
@@ -5,19 +5,23 @@ trap "cd '$PWD'" EXIT
 cd "$1"
 
 git status -sb
+echo '----------------'
 
 git add .
 echo
 message="${@:2}"
 git commit -m "$message"
 echo
+echo '----------------'
 
 git status -sb
 echo
+echo '----------------'
 
 source ~/.sh_functions
-git_log -1 --stat
-sleep 2
+git_log -2 --stat
+echo '----------------'
 
+sleep 2
 git push
 
