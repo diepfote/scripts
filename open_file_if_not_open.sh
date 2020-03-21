5cc5059abde53f6cfe42905e5cc77b9844c20de5
Florian Begusch <florian.begusch@gmail.com>
2020-03-21 20:31:32 +0100

    Generalize open_pdf_file_if_not_open script

--- open_file_if_not_open.sh
+++ open_file_if_not_open.sh
@@ -2,7 +2,7 @@ open_file_if_not_open ()
 {
   filename="$1"
 
-  if [ -z "$(ps -ef | grep -v grep | grep zathura | grep "$(basename "$filename")")" ]; then
+  if [ -z "$(ps -ef | grep -v grep | grep "$(basename "$filename")")" ]; then
     xdg-open "$filename"  1>/dev/null 2>/dev/null
   fi
 }
