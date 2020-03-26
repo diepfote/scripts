bbeb835d3e12cac18c9c66170192d821f55ff837
Florian <florian.begusch@gmail.com>
2020-03-26 09:15:16 +0100

    Check Boxcryptor version in build script

--- build_boxcryptor.sh
+++ build_boxcryptor.sh
@@ -8,12 +8,19 @@ shopt -s failglob  # error on unexpaned globs
 
 
 trap "popd" EXIT
-
 pushd ~/Documents/boxcryptor_container
 
-curl -C - -L 'https://ptc.secomba.com/api/boxcryptor/linuxPortable/latest' -o Boxcryptor.tar.gz
+
+download_url="$(curl -s 'https://ptc.secomba.com/api/boxcryptor/linuxPortable/latest' | grep tar | sed -r 's#.*https#https#')"
+
+sed_command='s#.*_##;s#\.tar\.gz##'
+upstream_version="$(echo "$download_url" | sed -r "$sed_command")"
+file_version="$(ls Boxcryptor* | sed -r "$sed_command")"
 
 set -x
+[ "$upstream_version" != "$file_version" ] && rm Boxcryptor*
+curl -C - -sO "$download_url"
+
 podman build --no-cache -t boxcryptor:0.1 .
 set +x
 
