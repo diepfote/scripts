6c24c582e89ade04bf8599eae76ce1912267e912
Florian Begusch <florian.begusch@gmail.com>
2020-03-30 20:08:31 +0200

    Report krew pkgs

--- report-brew-pkgs.sh
+++ report-brew-pkgs.sh
@@ -14,6 +14,7 @@ dir=~/Documents/misc/mac-os
 #brew info --installed --json | jq .[].name | sed 's#"##g' > ~/Documents/misc/mac-os/brew-pkgs.txt
 brew list > "$dir"/brew-pkgs.txt
 brew cask list > "$dir"/brew-cask-pkgs.txt
+kubectl krew list > "$dir"/krew-pkgs.txt
 
 username="$(read_toml_setting ~/Documents/config/fastmail.conf username)"
 rclone sync --exclude .DS_Store --delete-excluded -v "$dir" 'fastmail:'$username'.fastmail.com/files/-configs/mac-os'
