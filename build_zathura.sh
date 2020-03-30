7fb0be342dae5066bccb0778c000d0bfe169d4f5
Florian <florian.begusch@gmail.com>
2020-03-30 13:09:57 +0200

    Add build script for zathura container

---
+++ build_zathura.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
+
+trap "popd" EXIT
+pushd ~/Documents/dockerfiles/zathura
+
+set -x
+podman build --squash --no-cache -t zathura:0.1 .
+set +x
+
