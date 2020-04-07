b1871f821553769d81c99a8fb17b5a3bbccfd786
Florian Begusch <florian.begusch@gmail.com>
2020-04-07 07:23:24 +0200

    Add set example

---
+++ set-example_last-argument.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
+echo -e "${@:1:3}"
+
+#array=( "$@" )
+#unset "array[${#array[@]}-1]"    # Removes last element -- also see: help unset
+#set "$array"
+set -- "${@:1:$(($#-1))}"
+
+echo $@
+
