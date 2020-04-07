344a9c87dc0ad5e5e4126f835eef08a8e3686743
Florian Begusch <florian.begusch@gmail.com>
2020-04-07 07:23:14 +0200

    Download files in mac-os config first (report-brew-pkgs)

--- report-brew-pkgs.sh
+++ report-brew-pkgs.sh
@@ -11,11 +11,13 @@ source ~/.sh_functions
 dir=~/Documents/misc/mac-os
 [ ! -d "$dir" ] && mkdir "$dir"
 
+username="$(read_toml_setting ~/Documents/config/fastmail.conf username)"
+rclone sync --exclude .DS_Store --delete-excluded -v 'fastmail:'$username'.fastmail.com/files/-configs/mac-os' "$dir"
+
 #brew info --installed --json | jq .[].name | sed 's#"##g' > ~/Documents/misc/mac-os/brew-pkgs.txt
 brew list > "$dir"/brew-pkgs.txt
 brew cask list > "$dir"/brew-cask-pkgs.txt
 kubectl krew list > "$dir"/krew-pkgs.txt
 
-username="$(read_toml_setting ~/Documents/config/fastmail.conf username)"
 rclone sync --exclude .DS_Store --delete-excluded -v "$dir" 'fastmail:'$username'.fastmail.com/files/-configs/mac-os'
 
