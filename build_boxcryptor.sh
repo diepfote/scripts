e4c24362c64f11b464b88b69918d4441d4c352ed
Florian <florian.begusch@gmail.com>
2020-04-11 15:32:57 +0200

    Improve log output for build_boxcryptor

--- build_boxcryptor.sh
+++ build_boxcryptor.sh
@@ -14,10 +14,14 @@ pushd ~/Documents/dockerfiles/boxcryptor
 download_url="$(curl -s 'https://ptc.secomba.com/api/boxcryptor/linuxPortable/latest' | grep tar | sed -r 's#.*https#https#')"
 
 sed_command='s#.*_##;s#\.tar\.gz##'
+set -x
 upstream_version="$(echo "$download_url" | sed -r "$sed_command")"
+set +x
 
 shopt -u failglob  # unset error on unexpaned globs
+set -x
 file_version="$(ls Boxcryptor* | sed -r "$sed_command" || true )"
+set +x
 shopt -s failglob  # error on unexpaned globs
 
 if [ "$upstream_version" != "$file_version" ]; then
