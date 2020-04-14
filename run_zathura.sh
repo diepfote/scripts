ed85e49d83f9e767d2c7ddc0a082fcb68a27d2f3
Florian Begusch <florian.begusch@gmail.com>
2020-04-14 19:27:20 +0200

    Add host mounts to run_zathura

--- run_zathura.sh
+++ run_zathura.sh
@@ -6,18 +6,24 @@ set -e  # exit on non-zero return value
 #set -f  # disable globbing/filename expansion
 shopt -s failglob  # error on unexpaned globs
 
+[ -z "$*" ] && command=bash || command=$@
+
+
 ip="$(ifconfig en0  | grep -E 'inet\b' | sed 's#.*inet ##;s# netmask.*##')"
 xhost + "$ip"
 
-docker run -it \
-  -v ~/Documents/books\&documentation:/data1:ro \
-  -v ~/Downloads:/data2:ro \
-  -w /data1 \
+docker run \
+  -v ~/Documents/books\&documentation:/books:ro \
+  -v ~/Downloads:/downloads:ro \
+  -v ~/Documents/cheatsheets:/cheatsheets:ro \
+  -w /cheatsheets \
   -e DISPLAY="$ip":0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
   --security-opt=no-new-privileges \
   --cap-drop=ALL \
-  --rm --name zathura \
+  --rm \
+  -it \
+  --name zathura \
   zathura:0.1 \
-  bash
+  $command
 
