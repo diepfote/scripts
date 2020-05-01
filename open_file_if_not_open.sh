ba4aa7e722640c8e0e8cc46ced4f3f29a4f83f32
Florian <florian.begusch@gmail.com>
2020-05-01 10:49:44 +0200

    Minor improvement of open_file_if_not_open

--- open_file_if_not_open.sh
+++ open_file_if_not_open.sh
@@ -1,6 +1,6 @@
 open_file_if_not_open ()
 {
-  filename="$1"
+  local filename="$1"
 
   if [ -z "$(ps -ef | grep -v grep | grep "$(basename "$filename")")" ]; then
     xdg-open "$filename"  1>/dev/null 2>/dev/null
