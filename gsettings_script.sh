24a98569fc54b8b0a94e5cf8992bbede508e1416
Florian <florian.begusch@gmail.com>
2020-06-09 22:56:51 +0200

    Update script to persist gsettings

--- gsettings_script.sh
+++ gsettings_script.sh
@@ -8,10 +8,13 @@ gsettings set org.cinnamon.desktop.default-applications.terminal exec uxterm
 gsettings set org.gnome.desktop.media-handling automount false
 gsettings set org.gnome.desktop.wm.keybindings raise "['F1']"
 
-# nemo / nautlis
+# nemo / nautilus
 gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.nemo.desktop show-desktop-icons true
 
 # disable sleep if on ac power
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+
+# wayland - allow applications to "grab" the keyboard
+gsettings set org.gnome.mutter.wayland xwayland-grab-access-rules "['VirtualBox Machine']"
 
