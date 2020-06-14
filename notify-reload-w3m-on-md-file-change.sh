033510c1ade647e47c21084304738a0a3d739138
Florian <florian.begusch@gmail.com>
2020-06-14 21:32:40 +0200

    Cleanup write_markdown logic

--- notify-reload-w3m-on-md-file-change.sh
+++ notify-reload-w3m-on-md-file-change.sh
@@ -33,18 +33,7 @@ set -e  # exit on non-zero return value
 shopt -s failglob  # error on unexpaned globs
 
 
-_get_w3m_pane_id() {
-
-  local pane_to_reload="$(for pane_pid in $(tmux list-panes -F '#{pane_pid}'); do \
-    ps -f --ppid "$pane_pid" \
-    | awk '{ print substr($0, index($0,$8))}' \
-    | grep /w3m 1>/dev/null && itis=true; \
-      set +u; [ "$itis" = true ] \
-      && echo "$pane_pid"; itis=false; set -u ; done)"
-
-  tmux list-panes -F "#{pane_pid} #{pane_id}" \
-    | grep "$pane_to_reload" | cut -d ' ' -f2
-}
+source ~/Documents/scripts/source-me_common-functions.sh
 
 watch_dir="$1"
 
--- source-me_common-functions.sh
+++ source-me_common-functions.sh
@@ -16,6 +16,23 @@ die()
 }
 
 
+_get_w3m_pane_id() {
+
+   local pane_to_reload="$(for pane_pid in $(tmux list-panes -F '#{pane_pid}'); do \
+    ps -f --ppid "$pane_pid" \
+    | awk '{ print substr($0, index($0,$8))}' \
+    | grep /w3m 1>/dev/null && itis=true; \
+      set +u; [ "$itis" = true ] \
+      && echo "$pane_pid"; itis=false; set -u ; done)"
+
+   pane_ids=$(tmux list-panes -F "#{pane_pid} #{pane_id}" \
+     | grep "$pane_to_reload" | cut -d ' ' -f2)
+
+   [ "$(echo $pane_ids | tr ' ' '\n' | wc -l)" -lt 2 ] && \
+     echo "$pane_ids"
+}
+
+
 _complete_files_and_dirs_helper () {
   # completion file for bash
 
