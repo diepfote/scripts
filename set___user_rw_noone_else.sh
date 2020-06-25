0e06a6d29ccf1f40722171b39a526ef633ef46a1
Florian <florian.begusch@gmail.com>
2020-06-25 14:27:27 +0200

    Do not inject filenames in find exec

--- set___user_rw_noone_else.sh
+++ set___user_rw_noone_else.sh
@@ -10,6 +10,6 @@ if [ -z "$dir" ]; then
   dir=.
 fi
 
-find "$dir" -type f -exec sh -c 'chmod -f 600 "{}"' \; || true  # do not return non-zero return code
-find "$dir" -type d -exec sh -c 'chmod -f 700 "{}"' \; || true
+find "$dir" -type f -exec sh -c 'chmod -f 600 "$0"' {} \; || true  # do not return non-zero return code
+find "$dir" -type d -exec sh -c 'chmod -f 700 "$0"' {} \; || true
 
