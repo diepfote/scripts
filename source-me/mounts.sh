1395890741fa125f65aca6ef16f38cdf08b342bf
Florian <florian.begusch@gmail.com>
2020-06-28 06:59:23 +0200

    Move mount_vdi and mount_iso to source-me/mounts.sh

--- source-me/mounts.sh
+++ source-me/mounts.sh
@@ -2,6 +2,8 @@
 
 source ~/Documents/scripts/source-me/common-functions.sh
 source ~/Documents/scripts/source-me/progressbar.sh
+source ~/.sh_functions
+
 
 mount_luks_device ()
 {
@@ -17,10 +19,7 @@ mount_luks_device ()
   sudo mkdir "$mount_point" || true  # dir exists
   sudo mount /dev/mapper/"$mapper_name" "$mount_point"
 
-  tmux split-window -d -v
-  tmux send-keys -t .+ "n $mount_point" C-m
-  tmux selectp -t .+
-
+  n-for-dir-in-tmux-pane-below "$mount_point"
   progressbar 'waiting to close encrypted drive'
 
   while true; do
@@ -29,3 +28,49 @@ mount_luks_device ()
   done
 }
 
+mount_vdi ()
+{
+  local vdi_file="$1"
+  local mount_partition="$2"
+  [[ -z "$mount_partition" ]] && local mount_partition=2
+
+  local temp="$(mktemp -d)"
+
+  trap "sudo umount '$temp'; rm -rf '$temp'; sudo qemu-nbd -d /dev/nbd0" EXIT
+
+  sudo modprobe -v nbd
+
+  set -x
+  # mount as disk
+  sudo qemu-nbd -c /dev/nbd0 "$vdi_file"
+
+  # mount partition
+  echo -en "$PURPLE"; echo -e "[?] attempting to mount partition 2 (might have to be changed).$NC"
+  sudo mount /dev/nbd0p"$mount_partition" "$temp"
+  set +x
+
+  n-for-dir-in-tmux-pane-below "$temp"
+  progressbar 'waiting to close vdi file'
+
+  while true; do
+    sudo -v
+    sleep 60
+  done
+}
+
+mount_iso ()
+{
+  local file="$1"
+  local temp="$(mktemp -d)"
+
+  sudo mount -o loop  "$file" "$temp"
+  trap "sudo umount "$temp"; rm -r "$temp"" EXIT
+
+  n-for-dir-in-tmux-pane-below "$temp"
+  progressbar 'waiting to close iso file'
+
+  while true; do
+    sudo -v
+    sleep 60
+  done
+}
