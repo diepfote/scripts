306d94189d550e6b3771588e67e751f4c3d011a1
Florian <florian.begusch@gmail.com>
2020-07-04 21:41:07 +0200

    Update NNN_BMS

--- source-me/posix-compliant-shells.sh
+++ source-me/posix-compliant-shells.sh
@@ -6,7 +6,7 @@ readonly LESSSECURE
 
 export NNN_COLORS=2136  # use a different color for each context -> version >= 3.2
 export NNN_TRASH=1
-export NNN_BMS='d:~/Documents;h:~;D:~/Downloads;f:~/.config/fish/functions;s:~/Documents/scripts;L:~/Library/LaunchAgents;l:~/Documents/systemd-user;S:~/.config/systemd/user;E:/etc/'  # jump locations for nnn
+export NNN_BMS="d:~/Documents;h:~;D:~/Downloads;f:~/.config/fish/functions;s:~/Documents/scripts;L:~/Library/LaunchAgents;l:~/Documents/systemd-user;S:~/.config/systemd/user;E:/etc/;v:~/Videos;V:/run/media/$USER/large_drive/Media/Video-Files/Videos;m:~/Movies"  # jump locations for nnn
 export NNN_FIFO=/tmp/nnn.fifo
 export NNN_PLUG='p:preview-tui'  # curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
 
