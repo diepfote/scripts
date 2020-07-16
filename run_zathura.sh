5b67741a1d818a02d4dabaa337ef04708b690cf0
Florian Begusch <florian.begusch@gmail.com>
2020-07-16 19:34:34 +0200

    Fix zathura not remembering location

--- run_zathura.sh
+++ run_zathura.sh
@@ -13,10 +13,13 @@ ip="$(ifconfig en0  | grep -E 'inet\b' | sed 's#.*inet ##;s# netmask.*##')"
 xhost + "$ip"
 
 docker run \
+  -v ~/.config/zathura:/root/.config/zathura \
+  -v ~/.local/share/zathura:/root/.local/share/zathura \
+  -v ~/.bash_history:/root/.bash_history \
   -v ~/Documents/books\&documentation:/books:ro \
   -v ~/Downloads:/downloads:ro \
   -v ~/Documents/cheatsheets:/cheatsheets:ro \
-  -w /cheatsheets \
+  -w /books \
   -e DISPLAY="$ip":0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
   --security-opt=no-new-privileges \
