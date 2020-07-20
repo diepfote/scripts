26ea39091b93f56b0cb3e62f881626418b9eb71c
Florian Begusch <florian.begusch@gmail.com>
2020-07-20 12:49:49 +0200

    Add build_yay and run_yay

---
+++ run_yay.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
+[ -z "$*" ] && command=bash || command=$@
+
+
+ip="$(ifconfig en0  | grep -E 'inet\b' | sed 's#.*inet ##;s# netmask.*##')"
+xhost + "$ip"
+
+docker run \
+  -u build-user \
+  -v ~/.bash_history:/root/.bash_history \
+  -e DISPLAY="$ip":0 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  --rm \
+  -it \
+  --name yay \
+  yay \
+  $command
+
