8f0039574c99f5fd6c1389b467a3bdbdd8498b6c
Florian Begusch <florian.begusch@gmail.com>
2020-08-17 21:58:01 +0200

    Keep more fish and bash backup files

--- normal-privileges_systemd_scripts/bash_backup.sh
+++ normal-privileges_systemd_scripts/bash_backup.sh
@@ -9,8 +9,8 @@ user_dir="$HOME"
 bash_backup_dir="$user_dir/.bash_backup"
 [[ -d "$bash_backup_dir" ]] || mkdir "$bash_backup_dir"
 
-# Remove 10th file
-trap "rm -f $bash_backup_dir/$(ls -t $bash_backup_dir | sed -n 10p) 2>/dev/null" EXIT
+# Remove 30th file
+trap "rm -f $bash_backup_dir/$(ls -t $bash_backup_dir | sed -n 30p) 2>/dev/null" EXIT
 
 # Get parent directory
 f_date=$(date +%FT%T%Z | sed 's/:/_/g')
--- normal-privileges_systemd_scripts/fish_backup.sh
+++ normal-privileges_systemd_scripts/fish_backup.sh
@@ -7,8 +7,8 @@ set -o pipefail
 fish_backup_dir=~/.local/share/fish/fish_backup
 [[ ! -d "$fish_backup_dir" ]] && mkdir "$fish_backup_dir"
 
-# Remove 10th file
-trap "rm -f $fish_backup_dir/$(ls -t $fish_backup_dir | sed -n 10p) 2>/dev/null" EXIT
+# Remove 30th file
+trap "rm -f $fish_backup_dir/$(ls -t $fish_backup_dir | sed -n 30p) 2>/dev/null" EXIT
 
 # Get parent directory
 fish_dir="$(dirname "$fish_backup_dir")"
