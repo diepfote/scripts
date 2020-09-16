492e20882cf7e2ce44712a9f2e656baae0b4238b
Florian Begusch <florian.begusch@gmail.com>
2020-09-16 11:01:56 +0200

    Add docker-prune-images automation script

---
+++ normal-privileges_systemd_scripts/docker-prune-images.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
+echo y | docker image prune -a --filter "until=170h"
