d4ecf4c901aeddcb5acff25dc81c481063bc30c9
Florian Begusch <florian.begusch@gmail.com>
2020-09-18 10:07:05 +0200

    Add script af-helm-weirdness

---
+++ af-helm-weirdness.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
+git checkout master
+git pull
+git checkout -b "$1"
+vault-decrypt
+git commit-do-not-push
+
