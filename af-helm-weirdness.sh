21a3c2c8265d00506809455682ae8d4fb07dc899
Florian Begusch <florian.begusch@gmail.com>
2020-09-23 10:44:50 +0200

    Improve robustness of af-helm-weirdness

--- af-helm-weirdness.sh
+++ af-helm-weirdness.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-set -o pipefail  # propagate errors
+# set -o pipefail  # propagate errors
 set -u  # exit on undefined
-set -e  # exit on non-zero return value
+# set -e  # exit on non-zero return value
 #set -f  # disable globbing/filename expansion
 shopt -s failglob  # error on unexpaned globs
 
 git checkout master
 git pull
-git checkout -b "$1"
+
+[ $# -lt 1 ] && set -- HEAD
+
+git checkout "$1" || git checkout -b "$1"
 vault-decrypt
 git commit-do-not-push
 
