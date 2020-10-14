5c973ad738b1c37d85f871b5df9ede85fa574c8a
Florian Begusch <florian.begusch@gmail.com>
2020-10-14 06:58:42 +0200

    Ensure source file is absolute path

--- progressbar.sh
+++ progressbar.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 
-source source-me/progressbar.sh
+source ~/Documents/scripts/source-me/progressbar.sh
 
 
-progressbar "emit some $PURPLE text$NC"
+progressbar "$1"
 read  # run progressbar until <Enter> is pressed
 
 kill %%  2>/dev/null  # kill last job; and do not emit an error message if the job is not found
