44987dc77543c10b37e0a186a7e9a58a6473d890
Florian Begusch <florian.begusch@gmail.com>
2020-11-03 08:23:18 +0100

    Cleanup tmux_info script

--- tmux_info.sh
+++ tmux_info.sh
@@ -11,73 +11,33 @@ _set_kubernetes_vars ()
   export KUBECONFIG="$(cat ~/._kubeconfig 2>/dev/null)"
   context="$(kubectl config current-context 2>/dev/null)"
   namespace="$(kubectl config get-contexts 2>/dev/null | grep \* | tr -s ' ' | cut -d ' ' -f5)"
-
-  minikube_running="$(ps -ef | grep -v grep | grep minikube)"
-  minikube_configured="$(kubectl config current-context 2>/dev/null | grep minikube)"
 }
 
 show_kubernetes_context ()
 {
-  _set_kubernetes_vars
-
-
   if [ -n "$context" ]; then
-    local output="($context)  "
+    echo -n "($context)  "
   fi
-
-  if [ -z "$minikube_configured" ]; then
-    echo -n "$output"
-  else
-    if [ ! -z "$minikube_running" ]; then
-      echo -n "$output"
-    fi
-  fi
-
 }
 
 show_kubernetes_namespace ()
 {
-  _set_kubernetes_vars
-
   if [ -n "$namespace" ]; then
-    local output=">$namespace< "
+    echo -n ">$namespace< "
   fi
-
-  echo -n "$output"
 }
-
-parse_git_branch()
-{
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
-}
-
-custom_git_ps1()
-{
-  #:
-  __git_ps1 | tr '(' '[' | tr ')' ']'
-}
-
 display_kubernetes_info()
 {
+  _set_kubernetes_vars
   echo -en "$(show_kubernetes_context)$(show_kubernetes_namespace)"
 }
 
 
-get_audio_muted()
-{
-  #if [ "$(uname)" = Darwin ]; then
-    ##echo -en "♩$(osascript -e 'output muted of (get volume settings)') | "
-    #:
-  #else
-    #echo -en "♩$(pactl list sinks | grep Mute | head -n 1 | cut -d ':' -f2) | "
-  #fi
-  :
-}
-
 display_tmux_info()
 {
-  echo -en "$(get_audio_muted)[ $(tmux_id) | $(display_kubernetes_info)] "
+  echo -en "[ $(tmux_id) | $(display_kubernetes_info)] "
 }
+
 
 display_tmux_info
 
