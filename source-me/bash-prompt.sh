5f39191a786583a760ba61ee21471d5048652e3f
Florian Begusch <florian.begusch@gmail.com>
2020-11-05 13:06:19 +0100

    Fix duplicating calls in PROMP_COMMAND

--- source-me/bash-prompt.sh
+++ source-me/bash-prompt.sh
@@ -158,8 +158,18 @@ gen_ps1 () {
 }
 
 unset PS1
+# !! remember to escape dollar signs, otherwise PS1 caches the output !!
 # export PS1="[ $LIGHT_GREEN\w$NC ]\n$ "
 
 # run refresh_tmux_kubecontext in subshell to supress job output
-export PROMPT_COMMAND="gen_ps1; (refresh_tmux_openstack_and_kubecontext); source ~/.sh_functions; history -a; history -n; $PROMPT_COMMAND"
+unset PROMPT_COMMAND
+export PROMPT_COMMAND="gen_ps1; (refresh_tmux_openstack_and_kubecontext); source ~/.sh_functions; history -a; history -n"
+
+######## leave these ↓ here - might modify PROMPT_COMMAND
+
+[[ -x kubectl ]] && source <(kubectl completion bash)
+
+eval "$(direnv hook bash 2>/dev/null || true)"
+
+eval "$(gh completion -s bash 2>/dev/null || true)"
 
