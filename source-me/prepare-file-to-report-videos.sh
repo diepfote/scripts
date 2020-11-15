4b35c4d7b033c92f0cd85d19199612bbb2cb2ad2
Florian Begusch <florian.begusch@gmail.com>
2020-11-15 17:55:26 +0100

    Fix report-videos support script

--- source-me/prepare-file-to-report-videos.sh
+++ source-me/prepare-file-to-report-videos.sh
@@ -3,7 +3,13 @@ write_current_videos_to_file()
   local dir="$1"
   local filename="$2"
 
-  [ "$(uname)" = Darwin ] && local word=Movies || local word=Videos
+  if [ "$(uname)" = Darwin ]; then
+    local word=Movies
+    local hist_file=~/.bash_history
+  else
+    local word=Videos
+    local hist_file=~/.local/share/fish/fish_history
+  fi
   local path_to_search="$HOME/$word"
 
   find "$path_to_search" -path "$path_to_search/watched" -prune -o -name '*.mp4' -exec sh -c \
@@ -11,7 +17,6 @@ write_current_videos_to_file()
     print(\"- \" + name.split(\"'"$word"'/\")[1]) if len(end) == 11 else print(end=\"\")" "$end" "{}"' \; > "$dir"/"$filename"
 
   echo >> "$dir"/"$filename"
-  [ "$(uname)" = Darwin ] && history | grep -E '^mpv ' | uniq | head -n 15 >> "$dir"/"$filename" || \
-                             fish -c 'history | grep -E "^mpv " | uniq | head -n 15' >> "$dir"/"$filename"
+  grep -E "^mpv " "$hist_file" | uniq | tail -n 15 >> "$dir"/"$filename"
 }
 
