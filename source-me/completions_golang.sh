5c31572fd83a2ed648c36069cf71584d131dac7d
Florian Begusch <florian.begusch@gmail.com>
2020-11-23 22:31:32 +0100

    Add golang tools completions

---
+++ source-me/completions_golang.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+script_dir="$HOME/Documents/scripts"
+source "$script_dir/source-me/common-functions.sh"
+
+_golang_complete ()
+{
+  export DIR_TO_COMPLETE="$HOME/Documents/golang/tools"
+  _complete_files_and_dirs
+}
+
+complete -o filenames -F _golang_complete new-go
+complete -o filenames -F _golang_complete edit-go
+complete -o filenames -F _golang_complete cp-go
+complete -o filenames -F _golang_complete mv-go
+complete -o filenames -F _golang_complete rm-go
+complete -o filenames -F _golang_complete checkout-go
+
