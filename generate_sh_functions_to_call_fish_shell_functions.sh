66f9ead270e5454e2b47a890992270478a232bcc
Florian <florian.begusch@gmail.com>
2020-12-04 21:11:41 +0100

    play_dvd_iso generate_sh.

--- generate_sh_functions_to_call_fish_shell_functions.sh
+++ generate_sh_functions_to_call_fish_shell_functions.sh
@@ -57,7 +57,7 @@ for file in $(find ~/.config/fish/functions/ -name "*.fish"); do
     echo 'function '"$basename_no_ext"' {  fish -c "'"$basename_no_ext" ''\''$1'\''"'\; } >> "$aliases_file"
   elif [[ "$basename_no_ext" =~ cheat ]]; then
     echo 'function '"$basename_no_ext"' {  fish -c "'"$basename_no_ext"' \"$1\" \"$2\" "'\; } >> "$aliases_file"
-  elif [[ "$basename_no_ext" =~ mpv ]]; then
+  elif [[ "$basename_no_ext" =~ mpv|play_dvd_iso ]]; then
     echo 'function '"$basename_no_ext"' {  fish -c "'"$basename_no_ext"' \"$1\" "'\; } >> "$aliases_file"
   else
     echo 'function '"$basename_no_ext"' {  commands="$@"; fish -c "'"$basename_no_ext"' $commands"'\; } >> "$aliases_file"
