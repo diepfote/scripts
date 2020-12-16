854527522664088a8100b5d32bf83cab0ce5be3a
Florian Begusch <florian.begusch@gmail.com>
2020-12-16 14:14:46 +0000

    Improve fish-to-bash

--- fish-to-bash.sh
+++ fish-to-bash.sh
@@ -6,10 +6,18 @@ set -e  # exit on non-zero return value
 #set -f  # disable globbing/filename expansion
 shopt -s failglob  # error on unexpaned globs
 
-sed -ri 's#\((.*)\)#"$(\1)"##' "$1"
-sed -ri 's#set ([A-Za-z_]+) #\1=##g' "$1"
-sed -ri 's#(if[^]]+ \])#\1; then#' "$1"
-sed -ri 's#\$argv\[([0-9]+)\]#"$\1"#' "$1"
-sed -ri 's#(\s+)end#\1fi#' "$1"
+temp_dir="$(mktemp -d)"
+new_file="$temp_dir/$(basename "$1")"
+cp "$1" "$new_file"
 
-~/Documents/scripts/work_repo_template.sh ~/.config/fish/functions git diff --word-diff
+# trap "rm -rf "$temp_dir" " EXIT
+
+sed -ri 's#\((.*)\)#"$(\1)"##' "$new_file"  # subshell
+sed -ri 's#set ([A-Za-z_]+) #\1=##g' "$new_file"  # set statements
+sed -ri 's#(if[^]]+ \])#\1; then#' "$new_file"  # if statements
+sed -ri 's#\$argv\[([0-9]+)\]#"$\1"#' "$new_file"  # argv
+sed -ri 's#(\s+)end#\1fi#' "$new_file"  # end
+# TODO for loop
+
+echo "$new_file"
+git diff --no-index "$1" "$new_file"
