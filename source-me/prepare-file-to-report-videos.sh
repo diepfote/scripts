1e67465ecd4ca566dd6794b96bf204f390538024
Florian Begusch <florian.begusch@gmail.com>
2021-01-12 21:38:32 +0000

    Fix tatze videos showing up in video reports

--- source-me/prepare-file-to-report-videos.sh
+++ source-me/prepare-file-to-report-videos.sh
@@ -14,6 +14,7 @@ write_current_videos_to_file()
   find "$path_to_search" \
     -path "$path_to_search/jonathan blow" -prune -o \
     -path "$path_to_search/watched" -prune -o \
+    -path "$path_to_search/***REMOVED***" -prune -o \
     -name '*.mp4' -exec sh -c \
     'end="$(echo "$0" | sed "s#.*-##;s#.mp4##")"; \
     python3 -c "import sys; end = sys.argv[1]; name = sys.argv[2]; print(\"- \" + name.split(\"'"$word"'/\")[1]) if len(end) == 11 else print(end=\"\")" "$end" "$0"' {} \; > "$dir"/"$filename"
