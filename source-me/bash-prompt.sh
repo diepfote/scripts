0f9b6039bb54124b8cac356659de793f0c60f303
Florian <florian.begusch@gmail.com>
2021-02-28 22:14:32 +0100

    Add i3status helper func to PROMPT_COMMAND

--- source-me/bash-prompt.sh
+++ source-me/bash-prompt.sh
@@ -23,6 +23,18 @@ export PROMPT_COMMAND="_ps1; source ~/.sh_functions; tmux refresh-client; histor
 
 if [ "$(uname)" = Darwin ]; then
   export PROMPT_COMMAND="(refresh_tmux_openstack_and_kubecontext); $PROMPT_COMMAND"
+else
+  __test_ping () {
+    local file=/tmp/tmp.ping-success
+    ping -c 1 -W 5 archlinux.org >/dev/null 2>&1
+    if [ "$?" -eq 0 ]; then
+      touch "$file"
+    else
+        rm "$file" 2>/dev/null
+    fi
+  }
+  export PROMPT_COMMAND="(__test_ping &); $PROMPT_COMMAND"
+
 fi
 
 ######## leave these ↓ here - might modify PROMPT_COMMAND
