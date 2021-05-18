e3157adb4a58257cc54f45e926fc4ea6cc3dfc13
Florian Begusch <florian.begusch@gmail.com>
2021-05-18 04:38:46 +0000

    Move bash templates to cheatsheets

--- bash-safe-template.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o pipefail  # propagate errors
-set -u  # exit on undefined
-set -e  # exit on non-zero return value
-#set -f  # disable globbing/filename expansion
-shopt -s failglob  # error on unexpaned globs
-
-
