703817018685bd234ec1f01ebfb8bce42030652f
Florian Begusch <florian.begusch@gmail.com>
2021-07-16 03:03:43 +0000

    Improve watch-namespace completions

--- source-me/darwin/completions_watch-namespace.sh
+++ source-me/darwin/completions_watch-namespace.sh
@@ -3,19 +3,10 @@
 # https://iridakos.com/programming/2018/03/01/bash-programmable-completion-tutorial
 
 
-
-_complete_namespaces () {
-  if [ -z "$all_namespaces" ]; then
-    read -r -d '' -a all_namespaces < <(oc get project -o json | jq '.items[].metadata.name' | sed 's#^"##;s#"$##')
-    # all_namespaces=('test-something-blub' 'test-something-minus')
-  fi
-
-  _print_namespaces () {
-    for name in "${all_namespaces[@]}"; do
-      echo "$name"
-    done
-  }
-  _print_namespaces
+_print_namespaces () {
+  for name in "${all_namespaces[@]}"; do
+    echo "$name"
+  done
 }
 
 _watch-namespace_completions()
@@ -25,9 +16,16 @@ _watch-namespace_completions()
   local prev_word="${COMP_WORDS["$COMP_CWORD"-1]}"
   local suggestions
 
+  if [ -z "$all_namespaces" ]; then
+    read -r -d '' -a all_namespaces < <(oc get project -o json | jq '.items[].metadata.name' | sed 's#^"##;s#"$##')
+    export all_namespaces
+
+    # all_namespaces=('test-something-blub' 'test-something-minus')
+fi
+
   case "${prev_word}" in
     -n)
-      COMPREPLY=($(compgen -W "$(_complete_namespaces)" -- "$cur_word"))
+      COMPREPLY=($(compgen -W "$(_print_namespaces)" -- "$cur_word"))
       ;;
     -r)
       COMPREPLY=($(compgen -W "9 10 12" -- "$cur_word"))
