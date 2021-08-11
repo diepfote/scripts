05595e690070de8fa80383014fd5e6a9a8183c75
Florian Begusch <florian.begusch@gmail.com>
2021-08-11 19:56:22 +0000

    Fix `oc get -n` completion outputs garbage

--- kubernetes/source-me/completions_watch-namespace.sh
+++ kubernetes/source-me/completions_watch-namespace.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
 # Structure nicked from:
 # https://devmanual.gentoo.org/tasks-reference/completion/index.html
 # https://iridakos.com/programming/2018/03/01/bash-programmable-completion-tutorial
 
+set +u
 
 _watch-namespace_completions()
 {
@@ -11,21 +13,26 @@ _watch-namespace_completions()
     done
   }
 
+  _complete-namespaces() {
+      if [ -z "$_all_namespaces" ]; then
+        read -r -d '' -a _all_namespaces < <(oc get project -o json | jq '.items[].metadata.name' | sed 's#^"##;s#"$##')
+      export _all_namespaces
+      fi
+
+    # _all_namespaces=('test-something-blub' 'test-something-minus')
+
+    COMPREPLY=($(compgen -W "$(_print "${_all_namespaces[@]}")" -- "$cur_word"))
+  }
+
   COMPREPLY=()
-  local cur_word="${COMP_WORDS["$COMP_CWORD"]}"
-  local prev_word="${COMP_WORDS["$COMP_CWORD"-1]}"
-  local suggestions
+  local cur_word prev_word
+
+  cur_word="${COMP_WORDS["$COMP_CWORD"]}"
+  prev_word="${COMP_WORDS["$COMP_CWORD"-1]}"
 
   case "${prev_word}" in
     -n)
-    if [ -z "$_all_namespaces" ]; then
-      read -r -d '' -a _all_namespaces < <(oc get project -o json | jq '.items[].metadata.name' | sed 's#^"##;s#"$##')
-      export _all_namespaces
-
-      # _all_namespaces=('test-something-blub' 'test-something-minus')
-fi
-
-      COMPREPLY=($(compgen -W "$(_print "${_all_namespaces[@]}")" -- "$cur_word"))
+      _complete-namespaces
       ;;
     -r)
       COMPREPLY=($(compgen -W "$(echo -e '9\n10\n12')" -- "$cur_word"))
