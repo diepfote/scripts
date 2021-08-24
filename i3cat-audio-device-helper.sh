918e7f06fa5637b5157429c58b96c689d2dc0532
Florian Begusch <florian.begusch@gmail.com>
2021-08-24 05:04:23 +0000

    cleanup i3cat-audio-device-helper.sh

--- i3cat-audio-device-helper.sh
+++ i3cat-audio-device-helper.sh
@@ -12,11 +12,18 @@ get_volume() {
   local device
   device="$1"
 
-  awk -F '[][]' '/%/ { print $2 }' <(amixer sget "$device") | head -n 1
+  awk -F '[][]' '/%/ { print $2 ; exit }' <(amixer sget "$device")
+}
+
+
+# muted = off
+# unmuted = on
+get_device_on_off () {
+  awk -F"[][]" '/%/ { print $4 ; exit }' <(amixer sget "$device")
 }
 
 while true; do
-  if [ "$(awk -F"[][]" '/%/ { print $4 }' <(amixer sget "$device"))" = off ]; then
+  if [ "$(get_device_on_off "$device")" = off ]; then
     i3cat encode --color '#f91bac' "$(get_volume "$device")"
   else
     i3cat encode "$(get_volume "$device")"
