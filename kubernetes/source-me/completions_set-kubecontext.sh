23b615b264fcb4f9bb73755079102bab30b1c9d8
Florian Begusch <florian.begusch@gmail.com>
2021-09-24 12:19:53 +0000

    Improve set-kubecontext completions

--- kubernetes/source-me/completions_set-kubecontext.sh
+++ kubernetes/source-me/completions_set-kubecontext.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+# complete -o filenames -F _complete_kubektx set-kubecontext
 
+script_dir="$HOME/Documents/scripts"
+source "$script_dir/source-me/common-functions.sh"
 
-complete -o filenames -F _complete_kubektx set-kubecontext
+_set-kubecontext_complete ()
+{
+  export DIR_TO_COMPLETE="$HOME/.kube"
+  _complete_files_and_dirs
+}
+
+complete -o filenames -F _set-kubecontext_complete set-kubecontext
+
