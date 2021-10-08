490d5312ab7b7e2d85d11c9e478e513f1fb553df
Florian Begusch <florian.begusch@gmail.com>
2021-10-08 21:48:37 +0200

    i3cat-audio-device-helper: handle USR1 signal to re-print device volume

--- i3cat-audio-device-helper.sh
+++ i3cat-audio-device-helper.sh
@@ -22,12 +22,24 @@ get_device_on_off () {
   awk -F"[][]" '/%/ { print $4 ; exit }' <(amixer sget "$device")
 }
 
-while true; do
+
+run () {
   if [ "$(get_device_on_off "$device")" = off ]; then
     i3cat encode --color '#f91bac' "♪: muted ($(get_volume "$device"))"
   else
     i3cat encode "♪: $(get_volume "$device")"
   fi
-  sleep .5
-done
+
+  sleep infinity &
+  echo "re-print volume -> $ kill -SIGUSR1 $!"  1>&2
+  wait "$!"
+}
+
+sigusr1 () {
+  run
+}
+
+trap sigusr1 USR1
+
+run
 
