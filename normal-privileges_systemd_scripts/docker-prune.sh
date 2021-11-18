3279039f1856504071e756f15404cf99265b0473
Florian Begusch <florian.begusch@gmail.com>
2021-11-18 10:52:09 +0000

    docker script cleanups

--- normal-privileges_systemd_scripts/docker-prune.sh
+++ normal-privileges_systemd_scripts/docker-prune.sh
@@ -6,6 +6,6 @@ set -e  # exit on non-zero return value
 #set -f  # disable globbing/filename expansion
 shopt -s failglob  # error on unexpaned globs
 
-echo y | docker image prune --filter "until=32h"
-echo y | docker container prune --filter "until=170h"
+docker image prune -f --filter "until=32h"
+docker container prune -f --filter "until=170h"
 
