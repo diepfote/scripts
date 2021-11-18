3b252930471cfe368ed9f048a2141218512060a8
Florian Begusch <florian.begusch@gmail.com>
2021-11-18 11:52:55 +0000

    Docker scripts cleanup

--- normal-privileges_systemd_scripts/docker-prune.sh
+++ normal-privileges_systemd_scripts/docker-prune.sh
@@ -6,6 +6,15 @@ set -e  # exit on non-zero return value
 #set -f  # disable globbing/filename expansion
 shopt -s failglob  # error on unexpaned globs
 
+if [ "$(uname)" = Darwin ]; then
+  set +u
+  set +e
+  export BASH_SOURCE_IT=true
+  source ~/.bashrc
+  set -e
+  set -u
+fi
+
 docker image prune -f --filter "until=32h"
 docker container prune -f --filter "until=170h"
-
+docker system prune -a -f
