92e02f1b8468273bff6980ce42bb5045cd5c3187
Florian Begusch <florian.begusch@gmail.com>
2022-01-08 10:06:14 +0100

    cleanup

--- non-privileged_systemd_scripts/noisy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-user=`cut -d : -f 1 /etc/passwd | grep flo | head -n 1`
-user_dir=$HOME
-
-cd $user_dir/Documents/pentesting_pkgs/noisy
-
-python noisy.py --config config.json
-
