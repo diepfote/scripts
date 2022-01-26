d50f0a6913c45182da762cd68c4b63e01bcfc6d2
Florian Begusch <florian.begusch@gmail.com>
2022-01-26 19:15:12 +0000

    cleanup

--- source-me/bash-prompt.sh
+++ source-me/bash-prompt.sh
@@ -14,18 +14,12 @@ _ps1 () {
   PS1="$(~/Documents/golang/tools/bash-helper/"$BASH_HELPER_FILENAME")"
 }
 
-# General comments on PROMPT_COMMAND
-#
-# 1) run refresh_tmux_kubecontext in subshell to supress job output
-# 2) sed ... is used to delete trailing whitespace presumably introduced
-#    by fzf ctrl-r completion
-#
+
 unset PROMPT_COMMAND
 export PROMPT_COMMAND="_ps1; source ~/.sh_functions; history -a; history -n"
 
 ######## leave these ↓ here - might modify PROMPT_COMMAND
-
+#
 eval "$(direnv hook bash 2>/dev/null || true)"
-
 eval "$(gh completion -s bash 2>/dev/null || true)"
 
