952f26ab50eab3e9e761a72336b5b3f6105ca36e
Florian Begusch <florian.begusch@gmail.com>
2022-01-31 10:39:12 +0000

    grep: exclude .pulumi

--- source-me/posix-compliant-shells.sh
+++ source-me/posix-compliant-shells.sh
@@ -72,6 +72,7 @@ alias ls='ls --color=auto'
 alias grep='grep --exclude-dir=.git \
                  --exclude-dir=.helm \
                  --exclude-dir=.tox \
+                 --exclude-dir=.pulumi \
                  --exclude-dir=.mypy_cache \
                  --exclude-dir=.eggs \
                  --exclude-dir=*.egg-info \
