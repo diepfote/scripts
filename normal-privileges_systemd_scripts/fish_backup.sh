36f8d11209aa8a9f8b2d739b2150f4a1ab4a21a0
Florian Begusch <florian.begusch@gmail.com>
2022-02-09 16:32:14 +0000

    Fix fish history backup

--- normal-privileges_systemd_scripts/fish_backup.sh
+++ normal-privileges_systemd_scripts/fish_backup.sh
@@ -7,16 +7,18 @@ set -o pipefail
 source ~/Documents/scripts/source-me/common-functions.sh
 
 
-backup_dir="$HOME/.bash_backup"
-[[ -d "$backup_dir" ]] || mkdir "$backup_dir"
-
 cleanup () {
   file_30="$(find-sorted "$backup_dir" | sed -n 30p)"
   rm -f "$file_30" 2>/dev/null
 }
 
-# Get parent directory
-fish_dir="$(dirname "$backup_dir")"
+fish_dir="$HOME/.local/share/fish"
+if [ ! -d "$fish_dir" ]; then
+  fish_dir="$HOME/.config/fish"
+fi
+backup_dir="$fish_dir/fish_backup"
+[[ -d "$backup_dir" ]] || mkdir "$backup_dir"
+
 f_date=$(date +%FT%T%Z | sed 's/:/_/g')
 
 cp "$fish_dir/fish_history" "$backup_dir/fish_history$f_date"
