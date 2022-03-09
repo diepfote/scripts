de280a791cdd1a12cbdcf7d1c4f82086de86ec9f
Florian Begusch <florian.begusch@gmail.com>
2022-03-09 17:21:54 +0000

    Extend default brew completions

--- source-me/darwin/completions_brew-extensions.sh
+++ source-me/darwin/completions_brew-extensions.sh
@@ -2,16 +2,17 @@
 
 _brew-extensions_completions()
 {
-  _print () {
-    for name in "$@"; do
-      echo "$name"
-    done
-  }
 
   index=$COMP_CWORD
   local cur_word="${COMP_WORDS[$index]}"
   prev_index=$((index - 1))
   local prev_word="${COMP_WORDS[$prev_index]}"
+
+  _print () {
+    for name in "$@"; do
+      echo "$name"
+    done
+  }
 
   if [ -z "$formulae" ]; then
     read -r -d '' -a formulae < <(brew list --formulae)
@@ -20,15 +21,22 @@ _brew-extensions_completions()
 
   COMPREPLY=()
   case "${prev_word}" in
-    *)
+    required-by|formulae-require)
       while IFS='' read -r line; do
         COMPREPLY+=("$line")
       done  < <(compgen -W "$(_print "${formulae[@]}")" -- "$cur_word")
       ;;
+    *)
+      # call function provided in /usr/local/etc/bash_completion.d/brew
+      # default completions
+      _brew
+    ;;
   esac
 }
 
 # TODO patch brew completions to show completions for the following two extensions:
 complete -F _brew-extensions_completions 'brew-required-by'
 complete -F _brew-extensions_completions 'brew-formulae-require'
+# add to default completions for brew
+complete -o bashdefault -o default -F _brew-extensions_completions brew
 
