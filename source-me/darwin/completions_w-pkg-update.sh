c2a16920f00c597a91b2663a491464352cfe5cd9
Florian Begusch <florian.begusch@gmail.com>
2022-04-01 04:30:16 +0000

    Cleanup w-pkg-update

--- source-me/darwin/completions_w-pkg-update.sh
+++ source-me/darwin/completions_w-pkg-update.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 _w-pkg-update_completions()
 {
   COMPREPLY=()
@@ -6,10 +8,22 @@ _w-pkg-update_completions()
   prev_index=$((index - 1))
   local prev_word="${COMP_WORDS[$prev_index]}"
 
+  _print() {
+    for elem in "$@"; do
+      echo "$elem"
+    done
+  }
+  completions=()
+  completions+=(-h)
+  completions+=(-g)
+  completions+=(-n)
+  completions+=(--no-lima)
+  completions+=(--no-disable-remote-login)
+  completions+=(--mac-os-updates-only)
 
   case "${prev_word}" in
     *)
-      COMPREPLY=($(compgen -W "$(echo -e '-g\n-r\n--disable-remote-login\n--no-lima\n-h')" -- "$cur_word"))
+      COMPREPLY=($(compgen -W  "$(_print "${completions[@]}")" -- "$cur_word"))
       ;;
   esac
 
