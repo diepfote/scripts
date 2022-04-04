451b9d84af6175f81d921360d558909b1183a690
Florian Begusch <florian.begusch@gmail.com>
2022-04-04 04:51:42 +0000

    Add tmux-resurrect cleanup

---
+++ normal-privileges_systemd_scripts/automate-tmux-resurrect-cleanup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
+files=()
+while read -r line; do
+  files+=("$line")
+done < <(find ~/.tmux/resurrect -type f -mtime 14)
+
+if [ "${#files[@]}" -lt 1 ]; then
+  exit
+fi
+
+echo "Clearing the following files:"
+for file in "${files[@]}"; do
+  echo "$file"
+done
