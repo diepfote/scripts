c697f7451dfdc12d62a1fb125990862be1e395bd
Florian Begusch <florian.begusch@gmail.com>
2023-08-10 05:42:41 +0000

    Add mpv watch_later directory to sync

--- normal-privileges_systemd_scripts/report-videos.sh
+++ normal-privileges_systemd_scripts/report-videos.sh
@@ -10,19 +10,30 @@ shopt -s failglob  # error on unexpaned globs
 source ~/Documents/scripts/source-me/common-functions.sh
 _add_to_PATH ~/Documents/python/tools/bin
 
-dir=~/Documents/misc/videos
-[ ! -d "$dir" ] && mkdir "$dir"
+local_video_syncer_storage=~/Documents/misc/videos
+mkdir -p "$local_video_syncer_storage"
 
 username="$(read_toml_setting ~/Documents/config/fastmail.conf username)"
 fastmail_path='fastmail:'"$username"'.fastmail.com/files/videos'
 
-if _rclone_verbose_sync_operation --delete-excluded "$fastmail_path" "$dir"; then
-  [ "$(uname)" = Darwin ] && filename=videos-work.txt \
-                          || filename=videos-home.txt
+if _rclone_verbose_sync_operation --delete-excluded "$fastmail_path" "$local_video_syncer_storage"; then
 
-  ~/Documents/scripts/bin/_prepare-file-to-report-videos "$dir/$filename"
+  video_syncer_file=videos-home.txt
+  system=arch
+  mpv_dir=~/.local/state/mpv/watch_later/
+  if [ "$(uname)" = Darwin ]; then
+    video_syncer_file=videos-work.txt
+    system=mac
+    mpv_dir=~/.config/mpv/watch_later/
+  fi
 
-  _rclone_verbose_sync_operation --delete-excluded "$dir" "$fastmail_path"
+  ~/Documents/scripts/bin/_prepare-file-to-report-videos "$local_video_syncer_storage/$video_syncer_file"
 
+  # sync video-syncer file
+  _rclone_verbose_sync_operation --delete-excluded "$local_video_syncer_storage" "$fastmail_path"
+
+  # sync mpv watch_later files
+  find "$mpv_dir" ! -mtime -180 -delete  # delete files older than 180 days
+  _rclone_verbose_sync_operation --delete-excluded "$mpv_dir" "$fastmail_path/$system-mpv-watch_later/"
 fi
 
