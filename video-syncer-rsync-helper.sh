e1580cb6f3433ae7fb7a1b36e0f15bf219fa7e4c
Florian Sorko <florian.begusch@gmail.com>
2023-10-02 06:06:02 +0000

    cleanup `video-syncer-rsync-helper.sh`

--- video-syncer-rsync-helper.sh
+++ video-syncer-rsync-helper.sh
@@ -8,6 +8,11 @@ set -e  # exit on non-zero return value
 shopt -s failglob  # error on unexpaned globs
 shopt -s inherit_errexit  # Bash disables set -e in command substitution by default; reverse this behavior
 
-echo "[remote] $1"
-echo "[local] $2"
-rsync --progress -av --exclude .DS_Store --exclude .localized --exclude no-sync/ -e 'ssh -i /home/flo/.ssh/mac-box-key' "$1" "$2"
+ssh_key="$1"
+remote="$2"
+local="$3"
+
+echo "[ssh key] $ssh_key"
+echo "[remote] $remote"
+echo "[local] $local"
+rsync --progress -av --exclude .DS_Store --exclude .localized --exclude no-sync/ -e "ssh -i $ssh_key" "$local" "$remote"
