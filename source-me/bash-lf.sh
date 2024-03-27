e88b608b293c1aaa5fdd950d06d04957441deb87
Florian Sorko <florian.begusch@gmail.com>
2024-03-27 19:14:42 +0000

    `lf`: Add `quit-to-cwd`

---
+++ source-me/bash-lf.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+lf () {
+  local f dir
+
+  command lf
+
+  f=/tmp/.lf-cwd
+
+  if [ -f "$f" ]; then
+      read -r dir < "$f"
+      # shellcheck disable=SC2164
+      cd "$dir"
+      rm "$f"
+  fi
+
+  n_empty-trash
+}
+
