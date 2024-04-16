866e4f28a1a40f479ad0952edad098bef95f1cd7
Florian Sorko <florian.begusch@gmail.com>
2024-04-16 16:10:25 +0000

    Fix i3 current time not updating

---
+++ i3cat-time-helper.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -o pipefail  # propagate errors
+set -u  # exit on undefined
+set -e  # exit on non-zero return value
+#set -f  # disable globbing/filename expansion
+shopt -s failglob  # error on unexpaned globs
+
+TZ="$1"
+
+print_time() {
+  i3cat encode < <(TZ="$TZ" date "+%a %m/%d %H:%M%:::z" | tr -d '\n')
+}
+
+
+run () {
+  print_time
+
+  sleep infinity &
+  # DO NOT PUT ANYTHING INBETWEEN THESE LINES.
+  # NOT EVEN SAVING PID AND RUNNING ANOTHER PROCESS
+  # like echo to print the PID.
+  #
+  # OTHERWISE YOU WILL BREAK THIS SCRIPTS SIGNAL
+  # HANDLING CAPABILITIES.
+  #
+  # Tests can be done via
+  #
+  # ps -ef | grep -F 'i3cat-time-helper.sh' | grep -v grep | awk '{ print $2 }' | xargs kill -SIGUSR1  # very broad (will kill vim if this file is open)
+  #
+  wait "$!"
+}
+
+sigusr1 () {
+  run
+}
+
+trap sigusr1 USR1
+
+run
+
