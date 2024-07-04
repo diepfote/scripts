693b2efd38b714d1e3eed279576019a74339ab59
Florian Sorko <florian.begusch@gmail.com>
2024-07-04 18:22:31 +0000

    Add python versions

--- source-me/darwin/posix-compliant-shells.sh
+++ source-me/darwin/posix-compliant-shells.sh
@@ -28,10 +28,25 @@ _add_to_MANPATH "/opt/homebrew/opt/gnu-getopt/share/man"
 # ADD THIS ONE BEFORE newer version --> export PATH="asdf:$PATH"
 # otherwise these python versions will be resolved before latest
 #
-# python 3.8
-python_38_path="/opt/homebrew/opt/python@3.8/libexec/bin"
+# python 3.9
+python_39_path="/opt/homebrew/opt/python@3.9/libexec/bin"
 _add_to_PATH "$python_38_path"
 
+# python 3.10
+python_310_path="/opt/homebrew/opt/python@3.10/libexec/bin"
+# ln -sf "$python_310_path"/python "$python_310_path"/python3
+_add_to_PATH "$python_310_path"
+# _add_to_PATH "$HOME/Library/Python/3.10/bin"
+# export LDFLAGS="-L/opt/homebrew/opt/python@3.10/lib:$LDFLAGS"
+# export PKG_CONFIG_PATH="/opt/homebrew/opt/python@3.10/lib/pkgconfig:$PKG_CONFIG_PATH"
+#
+# python 3.12
+python_312_path="/opt/homebrew/opt/python@3.12/libexec/bin"
+# ln -sf "$python_312_path"/python "$python_312_path"/python3
+_add_to_PATH "$python_312_path"
+# _add_to_PATH "$HOME/Library/Python/3.12/bin"
+# export LDFLAGS="-L/opt/homebrew/opt/python@3.12/lib:$LDFLAGS"
+# export PKG_CONFIG_PATH="/opt/homebrew/opt/python@3.12/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 # python 3.11 = default
 python_311_path="/opt/homebrew/opt/python@3.11/libexec/bin"
