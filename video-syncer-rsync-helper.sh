f99a82346e0d34b73ed1139935cd316eb60ff260
Florian Sorko <florian.begusch@gmail.com>
2024-07-09 09:09:20 +0000

    `video-sync`-helper: Prepare local disk sync

--- video-syncer-rsync-helper.sh
+++ video-syncer-rsync-helper.sh
@@ -8,11 +8,21 @@ set -e  # exit on non-zero return value
 shopt -s failglob  # error on unexpaned globs
 shopt -s inherit_errexit  # Bash disables set -e in command substitution by default; reverse this behavior
 
-ssh_key="$1"
-remote="$2"
-local="$3"
+ssh_opts=()
+if [ $# -eq 3 ]; then
+  ssh_key="$1"
+  remote="$2"
+  local="$3"
+  ssh_opts=(-e "ssh -i $ssh_key")
+elif [ $# -eq 2 ]; then
+  remote="$1"
+  local="$2"
+fi
 
 cleanup () { set +x; }
 trap cleanup EXIT
 set -x
-rsync --progress -av --exclude .DS_Store --exclude .localized --exclude no-sync/ -e "ssh -i $ssh_key" "$remote" "$local"
+# NOTE: ssh_opts may be empty -> local copy
+# remote will then just be a different folder on the same machine
+rsync --progress -av --exclude .DS_Store --exclude .localized --exclude no-sync/ "${ssh_opts[@]}" "$remote" "$local"
+
