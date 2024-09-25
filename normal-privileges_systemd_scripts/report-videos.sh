08ca8c24ec8ca2b8329a4089999daa3d15ab83de
Florian Sorko <florian.begusch@gmail.com>
2024-09-25 05:08:55 +0000

    Improve report-videos sync times

--- normal-privileges_systemd_scripts/report-videos.sh
+++ normal-privileges_systemd_scripts/report-videos.sh
@@ -49,40 +49,55 @@ touch "$LOCK_FILE"
 local_video_syncer_storage=~/Documents/misc/videos
 mkdir -p "$local_video_syncer_storage"
 
-remote_path='proton:videos'
+username="$(read_toml_setting ~/Documents/config/fastmail.conf username)"
+fastmail_path='fastmail:'"$username"'.fastmail.com/files/videos'
+proton_path='proton:videos'
 
 system=arch
+remote_system=mac
 mpv_dir=~/.local/state/mpv/watch_later/
 video_syncer_file=videos-home.txt
+video_syncer_file_remote=videos-work.txt
 if [ "$(uname)" = Darwin ]; then
   video_syncer_file=videos-work.txt
+  video_syncer_file_remote=videos-home.txt
   system=mac
+  remote_system=arch
   mpv_dir=~/.config/mpv/watch_later/
 fi
 
 
-# write mapping file
+# TODO run in parallel -> execute-in-repos trick
+# FETCH
+if ! rclone sync --checksum "$proton_path/$video_syncer_file_remote" "$local_video_syncer_storage"; then
+  exit 1
+fi
+if rclone sync --update "$fastmail_path/${remote_system}-mpv-watch_later/" "$local_video_syncer_storage/${remote_system}-mpv-watch_later/"; then
+  exit 1
+fi
+
+# ---------
+
+
+# cleanup redirection entries
+find "$mpv_dir" -size 17c -exec bash -c 'if grep -F "# redirect entry" "$0" >/dev/null; then rm "$0"; fi;' {} \;
+# sync mpv watch_later files
+find "$mpv_dir" ! -mtime -180 -delete  # delete files older than 180 days
+
 set -x
+# write videos-<system> file
+~/Documents/scripts/bin/_prepare-file-to-report-videos "$local_video_syncer_storage/$video_syncer_file"
+# write mpv watch-later mapping file
 ~/Documents/golang/tools/sync-video-syncer-mpv-watch-later-files/sync-video-syncer-mpv-watch-later-files create-mapping-file
 set +x
 
-
-if rclone sync --checksum --delete-excluded "$remote_path" "$local_video_syncer_storage"; then
-  # cleanup redirection entries
-  find "$mpv_dir" -size 17c -exec bash -c 'if grep -F "# redirect entry" "$0" >/dev/null; then rm "$0"; fi;' {} \;
-
-  ~/Documents/scripts/bin/_prepare-file-to-report-videos "$local_video_syncer_storage/$video_syncer_file"
-
-  # sync video-syncer file
-  rclone sync --checksum --delete-excluded "$local_video_syncer_storage" "$remote_path"
-
-  # sync mpv watch_later files
-  find "$mpv_dir" ! -mtime -180 -delete  # delete files older than 180 days
-  rclone sync --checksum --delete-excluded "$mpv_dir" "$remote_path/$system-mpv-watch_later/"
-fi
+# TODO run in parallel -> execute-in-repos trick
+# PUSH
+rclone sync --checksum --exclude  arch-mpv-watch_later --exclude mac-mpv-watch_later "$local_video_syncer_storage" "$proton_path"
+rclone sync --update "$mpv_dir" "$fastmail_path/${system}-mpv-watch_later/"
 
 
-# update watch_later config (if remote is further along)
+# update local watch_later config (if remote is further along)
 set -x
 ~/Documents/golang/tools/sync-video-syncer-mpv-watch-later-files/sync-video-syncer-mpv-watch-later-files --no-dry-run
 set +x
