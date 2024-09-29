4396d1840d2c4d4f624fb36144f7dfe865a30095
Florian Sorko <florian.begusch@gmail.com>
2024-09-29 03:52:37 +0000

    Fix video sync

--- normal-privileges_systemd_scripts/report-videos.sh
+++ normal-privileges_systemd_scripts/report-videos.sh
@@ -76,11 +76,12 @@ detect_error="$temp"/error
 # FETCH
 {
   echo '#!/usr/bin/env bash'
-  echo rclone sync --checksum "$proton_path/$video_syncer_file_remote" "$local_video_syncer_storage"
-  # echo 'echo exit code $?'
-  echo 'if [ $? -ne 0 ]; then echo proton > '"$detect_error"'; fi'
 
-  echo rclone sync --update "$fastmail_path/${remote_system}-mpv-watch_later/" "$local_video_syncer_storage/${remote_system}-mpv-watch_later/"
+  echo rclone sync --checksum "$proton_path/$video_syncer_file_remote"   "$local_video_syncer_storage"
+  # echo 'echo exit code $?'
+  echo 'if [ $? -ne 0 ]; then echo proton-video-file > '"$detect_error"'; fi'
+
+  echo rclone sync --update "$fastmail_path/${remote_system}-mpv-watch_later/"   "$local_video_syncer_storage/${remote_system}-mpv-watch_later/"
   # echo 'echo exit code $?'
   echo 'if [ $? -ne 0 ]; then echo fastmail > '"$detect_error"'; fi'
 } > "$exec_fetch"
@@ -88,7 +89,7 @@ chmod +x "$exec_fetch"
 
 # to execute 2 subtasks
 touch "$temp"/{1,2}
-execute-on-files -no-header -workers 8 -config <(ls "$temp"/{1,2}) "$exec_fetch"
+execute-on-files -no-header -workers 2 -config <(ls "$temp"/{1,2}) "$exec_fetch"
 
 if [ -e "$detect_error" ]; then
   echo "${RED}[!]$NC Fetch failed: $(cat "$detect_error") | Exiting early."
@@ -115,20 +116,25 @@ set +x
 exec_push="$temp/exec-push.sh"
 {
   echo '#!/usr/bin/env bash'
-  echo rclone sync --checksum --exclude  arch-mpv-watch_later --exclude mac-mpv-watch_later "$local_video_syncer_storage" "$proton_path"
-  # echo 'echo exit code $?'
-  echo 'if [ $? -ne 0 ]; then echo proton > '"$detect_error"'; fi'
 
-  echo rclone sync --update "$mpv_dir" "$fastmail_path/${system}-mpv-watch_later/"
+  echo rclone sync --checksum "$local_video_syncer_storage/$video_syncer_file"   "$proton_path"
+  # echo 'echo exit code $?'
+  echo 'if [ $? -ne 0 ]; then echo proton-video-file > '"$detect_error"'; fi'
+
+  echo rclone sync --checksum "$local_video_syncer_storage/mapping.txt"   "$proton_path"
+  # echo 'echo exit code $?'
+  echo 'if [ $? -ne 0 ]; then echo proton-mapping-file > '"$detect_error"'; fi'
+
+  echo rclone sync --update "$mpv_dir"   "$fastmail_path/${system}-mpv-watch_later/"
   # echo 'echo exit code $?'
   echo 'if [ $? -ne 0 ]; then echo fastmail > '"$detect_error"'; fi'
+
 } > "$exec_push"
 chmod +x "$exec_push"
-execute-on-files -no-header -workers 8 -config <(ls "$temp"/{1,2}) "$exec_fetch"
+execute-on-files -no-header -workers 2 -config <(ls "$temp"/{1,2}) "$exec_push"
 
 if [ -e "$detect_error" ]; then
   echo "${RED}[!]$NC Push failed: $(cat "$detect_error") | Continuing."
-  exit 11
 fi
 
 # update local watch_later config (if remote is further along)
