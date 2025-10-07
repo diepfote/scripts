31c04fbad6e8c4b2127f099508e81cab5cd416a5
Florian Sorko <florian.begusch@gmail.com>
2025-10-07 05:06:05 +0000

    Submit arch pkgs to rsync.net

--- normal-privileges_systemd_scripts/report-arch-pkgs.sh
+++ normal-privileges_systemd_scripts/report-arch-pkgs.sh
@@ -8,28 +8,28 @@ set -e  # exit on non-zero return value
 shopt -s failglob  # error on unexpaned globs
 
 
+cleanup () { set +x; }
+trap cleanup EXIT
+
+
 source ~/Repos/scripts/source-me/posix-compliant-shells.sh
 
 
 dir=~/.config/personal/sync-config/arch
 [ ! -d "$dir" ] && mkdir -p "$dir"
 
-username="$(read_toml_setting ~/.config/personal/fastmail.conf username)"
-remote_path='fastmail:'"$username"'.fastmail.com/files/-config/arch'
-# remote_path='proton:-configs/arch'
+remote_path='rsync.net:state/arch/'
 
-if rclone sync --delete-excluded "$remote_path" "$dir"; then
 
-  # backup pacman database
-  ## to restore move .tar.bz2 file to / and execute "tar -xjvf pacman_database.tar.bz2"
-  tar -cjf "$dir/pacman_database.tar.bz2" "/var/lib/pacman/local"
+# backup pacman database
+## to restore move .tar.bz2 file to / and execute "tar -xjvf pacman_database.tar.bz2"
+tar -cjf "$dir/pacman_database.tar.bz2" "/var/lib/pacman/local"
 
-  pacman -Qqem > "$dir/packages_explicit_external.txt"
-  pacman -Qqen > "$dir/packages_explicit_internal.txt"
-  pacman -Qqm  > "$dir/packages_all_external.txt"
-  pacman -Qqn  > "$dir/packages_all_internal.txt"
+pacman -Qqem > "$dir/packages_explicit_external.txt"
+pacman -Qqen > "$dir/packages_explicit_internal.txt"
+pacman -Qqm  > "$dir/packages_all_external.txt"
+pacman -Qqn  > "$dir/packages_all_internal.txt"
 
-  rclone sync --delete-excluded "$dir" "$remote_path"
-
-fi
+set -x
+rsync -av --delete "$dir" "$remote_path"
 
