c90fb7d3ed4457b3290524f9a12c6693b3edb99b
Florian Sorko <florian.begusch@gmail.com>
2025-11-25 07:48:51 +0000

    Use columns for color palette

--- 256-color-palette.sh
+++ 256-color-palette.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 for i in {0..255} ; do
-    echo -e "\x1b[38;5;${i}mcolour${i}"
-done
+  rem="$(echo "$i % 9" | bc)"
+  if [ "$rem" -eq 0 ]; then
+    echo
+  fi
 
+  echo -n -e "\x1b[38;5;${i}mcolour${i} "
+done
+echo
