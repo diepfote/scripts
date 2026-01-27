62f96e72bd89fe7321bef648882362362852dfa5
Florian Sorko <florian.begusch@gmail.com>
2026-01-27 19:39:49 +0000

    Add `pueue-nft-helper.sh`

---
+++ pueue-nft-helper.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+id="$1"
+result="$2"
+shift 2
+
+source ~/Repos/scripts/source-me/posix-compliant-shells.sh
+
+
+if [ "$result" = "Failed" ]; then
+  ntf send -t 'nc pq failure' "$id: $*" || echo "$id" > /tmp/pq-last-failed
+fi
+
