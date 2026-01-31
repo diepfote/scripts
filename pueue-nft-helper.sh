1eac11aba41252c67da1419a82451303df8baac6
Florian Sorko <florian.begusch@gmail.com>
2026-01-31 07:14:20 +0000

    Report hostname in pq helper

--- pueue-nft-helper.sh
+++ pueue-nft-helper.sh
@@ -6,7 +6,7 @@ shift 2
 source ~/Repos/scripts/source-me/posix-compliant-shells.sh
 
 
-if [ "$result" = "Failed" ]; then
-  ntf send -t 'nc pq failure' "$id: $*" || echo "$id" > /tmp/pq-last-failed
-fi
+# if [ "$result" = "Failed" ]; then
+  ntf send -t 'nc pq failure' "$(hostname): $id: $*" || echo "$id" > /tmp/pq-last-failed
+# fi
 
