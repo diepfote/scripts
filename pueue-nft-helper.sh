b0ed47efbd01273892ab36f98c55bf2b8f98b6fb
Florian Sorko <florian.begusch@gmail.com>
2026-01-31 18:13:03 +0000

    `pueue` helper: only notify on failure again

--- pueue-nft-helper.sh
+++ pueue-nft-helper.sh
@@ -6,7 +6,7 @@ shift 2
 source ~/Repos/scripts/source-me/posix-compliant-shells.sh
 
 
-# if [ "$result" = "Failed" ]; then
+if [ "$result" = "Failed" ]; then
   ntf send -t 'nc pq failure' "$(hostname): $id: $*" || echo "$id" > /tmp/pq-last-failed
-# fi
+fi
 
