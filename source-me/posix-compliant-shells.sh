41d73f0752849319aca79396b1eaa21e7b55a0ca
Florian Sorko <florian.begusch@gmail.com>
2026-02-16 21:32:32 +0000

    cleanup

--- source-me/posix-compliant-shells.sh
+++ source-me/posix-compliant-shells.sh
@@ -493,7 +493,6 @@ alias httpdump="sudo tcpdump -i \"\$_ngrep_interface\" -n -s 0 -w - | grep -a -o
 
 alias todo='vim ~/.filen/notes/.todo.txt'
 alias todow='vim ~/Desktop/todo/index.txt'
-alias voc='vim ~/.filen/notes/voc/'
 
 
 parrot-search () {
